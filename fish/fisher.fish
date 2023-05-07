1f601a46a63b17f085de74804dc8c692005aed4a
r0nsha <r0nsh4v1t@gmail.com>
2023-05-08 02:15:08 +0300

    fix fish plugins

--- fish/fisher.fish
+++ fish/fisher.fish
@@ -1,5 +1,4 @@
 if status is-interactive && ! functions --query fisher
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
-    fisher install jorgebucaran/fisher
     fisher update
 end
