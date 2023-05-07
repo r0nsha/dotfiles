48d6fa1072463f4d2d68b3c1275f8acfbbd324e8
r0nsha <r0nsh4v1t@gmail.com>
2023-05-07 23:02:06 +0300

    wip

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -27,13 +27,118 @@ fail () {
   exit
 }
 
-source_if_exists () {
-    if test -r "$1"; then
-        source "$1"
+link_file () {
+  local src=$1 dst=$2
+
+  local overwrite=
+  local backup=
+  local skip=
+  local action=
+
+  if [ -f "$dst" ] || [ -d "$dst" ] || [ -L "$dst" ]
+  then
+
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+    then
+
+      # ignoring exit 1 from readlink in case where file already exists
+      # shellcheck disable=SC2155
+      local currentSrc="$(readlink $dst)"
+
+      if [ "$currentSrc" == "$src" ]
+      then
+
+        skip=true;
+
+      else
+
+        user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
+        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -n 1 action  < /dev/tty
+
+        case "$action" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          b )
+            backup=true;;
+          B )
+            backup_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+
+      fi
+
+    fi
+
+    overwrite=${overwrite:-$overwrite_all}
+    backup=${backup:-$backup_all}
+    skip=${skip:-$skip_all}
+
+    if [ "$overwrite" == "true" ]
+    then
+      rm -rf "$dst"
+      success "removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]
+    then
+      mv "$dst" "${dst}.backup"
+      success "moved $dst to ${dst}.backup"
+    fi
+
+    if [ "$skip" == "true" ]
+    then
+      success "skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]  # "false" or empty
+  then
+    # ln -s "$1" "$2"
+    success "linked $1 to $2"
+  fi
+}
+
+install_dotfiles () {
+  info 'installing dotfiles'
+
+  local overwrite_all=false backup_all=false skip_all=false
+
+  find -H "$DOTFILES" -maxdepth 2 -name 'links.prop' -not -path '*.git*' | while read linkfile
+  do
+    cat "$linkfile" | while read line
+    do
+        local src dst dir
+        src=$(eval echo "$line" | cut -d '=' -f 1)
+        dst=$(eval echo "$line" | cut -d '=' -f 2)
+        dir=$(dirname $dst)
+
+        mkdir -p "$dir"
+        link_file "$src" "$dst"
+    done
+  done
+}
+
+create_env_file () {
+    if test -f "$HOME/.env.sh"; then
+        success "$HOME/.env.sh file already exists, skipping"
+    else
+        echo "export DOTFILES=$DOTFILES" > $HOME/.env.sh
+        success 'created ~/.env.sh'
     fi
 }
 
-source_if_exists $DOTFILES/
-
-echo "Updating submodules..."
+info "updating submodules..."
 git submodule update --init --recursive
+
+install_dotfiles
+create_env_file
+
+success 'All installed!'
