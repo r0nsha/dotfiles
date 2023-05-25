a2c5ec542179805d87d09839fa3d0bb916d8ceff
r0nsha <r0nsh4v1t@gmail.com>
2023-05-25 15:35:21 +0300

    macos

--- fish/macos.fish
+++ fish/macos.fish
@@ -3,12 +3,11 @@ switch (uname)
     case Darwin
         set -l machine (uname -m)
 
-        if [ test $machine = arm64 ]
-            # On ARM macOS
-            set HOMEBREW_PREFIX /opt/homebrew
-        else
-            # On Intel macOS
-            set HOMEBREW_PREFIX /usr/local
+        switch (uname -m)
+            case arm64
+                set HOMEBREW_PREFIX /opt/homebrew
+            case '*'
+                set HOMEBREW_PREFIX /usr/local
         end
 
         eval $HOMEBREW_PREFIX/bin/brew shellenv &>/dev/null
