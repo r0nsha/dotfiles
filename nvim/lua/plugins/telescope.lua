8b26191372efd00e1dd07572ff15aed3f7a01b27
r0nsha <r0nsh4v1t@gmail.com>
2023-06-16 16:06:00 +0300

    use fzy again

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -128,10 +128,10 @@ return {
     end,
   },
   {
-    "nvim-telescope/telescope-fzf-native.nvim",
+    "nvim-telescope/telescope-fzy-native.nvim",
     dependencies = { "nvim-telescope/telescope.nvim" },
     config = function()
-      require("telescope").load_extension "fzf_native"
+      require("telescope").load_extension "fzy_native"
     end,
   },
   {
