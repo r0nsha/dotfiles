e3c186c33ed5ae35b3c49dee84314c84ad49f231
r0nsha <r0nsh4v1t@gmail.com>
2023-06-21 19:15:39 +0300

    better descriptions

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,48 +1,44 @@
-local opts = { remap = false }
-
 -- Leader (Space)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Unmap leader key
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", opts)
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { remap = false })
 
--- Yank to clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', opts)
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { remap = false, desc = "Yank to clipboard" })
+vim.keymap.set("n", "<bar>", "<cmd>vsplit<cr>", { remap = false, desc = "Split window vertically" })
 
--- Paste from clipboard
-vim.keymap.set("n", "<leader>p", '"+p', opts)
+vim.keymap.set("n", "<leader>p", '"+p', { remap = false, desc = "Paste from clipboard" })
 
 -- Window mappings when tmux is not available
 if vim.fn.executable "tmux" ~= 1 then
-  vim.keymap.set("n", "<c-h>", "<c-w>h", opts)
-  vim.keymap.set("n", "<c-j>", "<c-w>j", opts)
-  vim.keymap.set("n", "<c-k>", "<c-w>k", opts)
-  vim.keymap.set("n", "<c-l>", "<c-w>l", opts)
+  vim.keymap.set("n", "<c-h>", "<c-w>h", { remap = false, desc = "Move Window: Left" })
+  vim.keymap.set("n", "<c-j>", "<c-w>j", { remap = false, desc = "Move Window: Down" })
+  vim.keymap.set("n", "<c-k>", "<c-w>k", { remap = false, desc = "Move Window: Up" })
+  vim.keymap.set("n", "<c-l>", "<c-w>l", { remap = false, desc = "Move Window: Right" })
 end
 
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- Move visual selection
-vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv", { remap = false, desc = "Move Visual Selection: Down" })
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv", { remap = false, desc = "Move Visual Selection: Up" })
 
 -- splitjoin
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mzJ`z", { remap = false, desc = "Splitjoin" })
 
 -- Justify center page up/down
-vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
-vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { remap = false })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { remap = false })
 
 -- Justify center search next/prev
-vim.keymap.set("n", "n", "nzzzv", opts)
-vim.keymap.set("n", "N", "Nzzzv", opts)
+vim.keymap.set("n", "n", "nzzzv", { remap = false })
+vim.keymap.set("n", "N", "Nzzzv", { remap = false })
 
 -- Don't put yanked objects when using 'c' or 'C'
-vim.keymap.set("n", "c", '"_c', opts)
-vim.keymap.set("n", "C", '"_C', opts)
+vim.keymap.set("n", "c", '"_c', { remap = false })
+vim.keymap.set("n", "C", '"_C', { remap = false })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
