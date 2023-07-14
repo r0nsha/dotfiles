c4941a0102fdebfc4040673a794d52058363b820
r0nsha <r0nsh4v1t@gmail.com>
2023-07-14 23:38:44 +0300

    add nvim-lint

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -183,6 +183,24 @@ return {
     end,
   },
   {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("lint").linters_by_ft = {
+        javascript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescript = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+      }
+
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
+  {
     "simrat39/rust-tools.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
