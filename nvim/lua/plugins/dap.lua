eacf9d8e6f83f751cdb31b2267ac4eb7f9b49795
r0nsha <r0nsh4v1t@gmail.com>
2023-08-09 12:06:09 +0300

    wip

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -35,8 +35,41 @@ return {
 
       -- Debug: Rust
 
+      dap.configurations.rust = {
+        {
+          name = "Launch",
+          type = "rt_lldb",
+          request = "launch",
+          cwd = "${workspaceFolder}",
+          program = "${workspaceFolder}/target/debug/${workspaceFolderBasename}",
+          stopOnEntry = false,
+          args = { "build" },
+
+          -- Types
+          initCommands = function()
+            -- Find out where to look for the pretty printer Python module
+            local rustc_sysroot = vim.fn.trim(vim.fn.system "rustc --print sysroot")
+
+            local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+            local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+            local commands = {}
+            local file = io.open(commands_file, "r")
+            if file then
+              for line in file:lines() do
+                table.insert(commands, line)
+              end
+              file:close()
+            end
+            table.insert(commands, 1, script_import)
+
+            return commands
+          end,
+        },
+      }
+
       -- https://github.com/simrat39/rust-tools.nvim/wiki/Use-with-dap.ext.vscode-launch.json
-      -- require("dap.ext.vscode").load_launchjs(nil, { rt_lldb = { "rust" } })
+      require("dap.ext.vscode").load_launchjs(nil, { rt_lldb = { "rust" } })
 
       -- Debug: Lua
       dap.configurations.lua = {
