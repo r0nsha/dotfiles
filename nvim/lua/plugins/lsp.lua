74d6d626f3d5f6145d0c1cfa38a5184ee24c51c9
r0nsha <r0nsh4v1t@gmail.com>
2023-08-09 11:31:46 +0300

    wip

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -214,6 +214,17 @@ return {
       local codelldb_path = extension_path .. "adapter/codelldb"
       local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
 
+      local this_os = vim.loop.os_uname().sysname
+
+      -- The path in windows is different
+      if this_os:find "Windows" then
+        codelldb_path = extension_path .. "adapter\\codelldb.exe"
+        liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+      else
+        -- The liblldb extension is .so for linux and .dylib for macOS
+        liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+      end
+
       -- https://github.com/simrat39/rust-tools.nvim/wiki/Use-with-dap.ext.vscode-launch.json
       require("dap.ext.vscode").load_launchjs(nil, { rt_lldb = { "rust" } })
 
