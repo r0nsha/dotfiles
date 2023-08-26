1f6b62a3a3c195b47e2b0bc29143044723791213
r0nsha <r0nsh4v1t@gmail.com>
2023-08-26 17:26:07 +0300

    wip

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -70,8 +70,8 @@ return {
 
             return {
               -- cmp-nvim
-              Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-              PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+              Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend },
+              PmenuSel = { fg = "none", bg = theme.ui.bg_p2 },
               PmenuSbar = { bg = theme.ui.bg_m1 },
               PmenuThumb = { bg = theme.ui.bg_p2 },
               -- transparent floats
