1eee7a8302e71e8c3fe08d3b5b5ebfbcd3a19887
r0nsha <r0nsh4v1t@gmail.com>
2023-08-26 15:41:46 +0300

    wip

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -67,6 +67,7 @@ return {
           },
           overrides = function(colors)
             local theme = colors.theme
+
             return {
               -- cmp-nvim
               Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
@@ -80,6 +81,14 @@ return {
               NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
               LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
               MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+              -- Telescope block-style
+              -- TelescopeTitle = { fg = theme.ui.special, bold = true },
+              -- TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+              -- TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+              -- TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+              -- TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+              -- TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+              -- TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
             }
           end,
           theme = "wave",
