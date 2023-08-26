4b9ed409796fa2035b2d165dc85e053e2018dcb3
r0nsha <r0nsh4v1t@gmail.com>
2023-08-26 17:21:23 +0300

    wip

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -57,10 +57,10 @@ return {
             theme = {
               all = {
                 ui = {
-                  bg_gutter = "none",
-                  float = {
-                    bg = "none",
-                  },
+                  -- bg_gutter = "none",
+                  -- float = {
+                  -- bg = "none",
+                  -- },
                 },
               },
             },
@@ -81,14 +81,6 @@ return {
               NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
               LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
               MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-              -- Telescope block-style
-              -- TelescopeTitle = { fg = theme.ui.special, bold = true },
-              -- TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-              -- TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-              -- TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-              -- TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-              -- TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-              -- TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
             }
           end,
           theme = "dragon",
