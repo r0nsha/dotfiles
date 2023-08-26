1dee09a3151d12e9478451a7ecac5dce57e6edb6
r0nsha <r0nsh4v1t@gmail.com>
2023-08-26 15:35:32 +0300

    wip

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,8 +5,8 @@ return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
-      require("gruvbox").setup { contrast = "dark", italics = false }
       if colorscheme == "gruvbox" then
+        require("gruvbox").setup { contrast = "dark", italics = false }
         vim.cmd.colorscheme "gruvbox"
       end
     end,
@@ -15,20 +15,20 @@ return {
     "Shatur/neovim-ayu",
     priority = 1000,
     config = function()
-      local mirage = false
-      local colors = require "ayu.colors"
-      colors.generate(mirage)
-
-      local ayu = require "ayu"
-
-      ayu.setup {
-        mirage = mirage,
-        overrides = function()
-          return { Comment = { fg = colors.comment } }
-        end,
-      }
-
       if colorscheme == "ayu" then
+        local mirage = false
+        local colors = require "ayu.colors"
+        colors.generate(mirage)
+
+        local ayu = require "ayu"
+
+        ayu.setup {
+          mirage = mirage,
+          overrides = function()
+            return { Comment = { fg = colors.comment } }
+          end,
+        }
+
         ayu.colorscheme()
       end
     end,
@@ -37,9 +37,8 @@ return {
     "EdenEast/nightfox.nvim",
     priority = 1000,
     config = function()
-      require("nightfox").setup()
-
       if colorscheme == "nightfox" then
+        require("nightfox").setup {}
         vim.cmd.colorscheme "nordfox"
       end
     end,
@@ -48,15 +47,15 @@ return {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
-      require("tokyonight").setup {
-        style = "storm",
-        styles = {
-          comments = { italic = false },
-          keywords = { italic = false },
-        },
-      }
-
       if colorscheme == "tokyonight" then
+        require("tokyonight").setup {
+          style = "storm",
+          styles = {
+            comments = { italic = false },
+            keywords = { italic = false },
+          },
+        }
+
         vim.cmd.colorscheme "tokyonight"
       end
     end,
@@ -65,58 +64,58 @@ return {
     "rebelot/kanagawa.nvim",
     priority = 1000,
     config = function()
-      require("kanagawa").setup {
-        compile = false,
-        undercurl = true,
-        commentStyle = { italic = false },
-        functionStyle = { italic = false },
-        keywordStyle = { italic = false },
-        statementStyle = { italic = false, bold = true },
-        typeStyle = { italic = false },
-        variablebuiltinStyle = { italic = false },
-        specialReturn = true,
-        specialException = true,
-        transparent = true,
-        dimInactive = false,
-        globalStatus = false,
-        terminalColors = true,
-        colors = {
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = "none",
-                float = {
-                  bg = "none",
+      if colorscheme == "kanagawa" then
+        require("kanagawa").setup {
+          compile = false,
+          undercurl = true,
+          commentStyle = { italic = false },
+          functionStyle = { italic = false },
+          keywordStyle = { italic = false },
+          statementStyle = { italic = false, bold = true },
+          typeStyle = { italic = false },
+          variablebuiltinStyle = { italic = false },
+          specialReturn = true,
+          specialException = true,
+          transparent = true,
+          dimInactive = false,
+          globalStatus = false,
+          terminalColors = true,
+          colors = {
+            theme = {
+              all = {
+                ui = {
+                  bg_gutter = "none",
+                  float = {
+                    bg = "none",
+                  },
                 },
               },
             },
           },
-        },
-        overrides = function(colors)
-          local theme = colors.theme
-          return {
-            -- cmp-nvim
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-            PmenuSbar = { bg = theme.ui.bg_m1 },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
-            -- transparent floats
-            NormalFloat = { bg = "none" },
-            FloatBorder = { bg = "none" },
-            FloatTitle = { bg = "none" },
-            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          }
-        end,
-        theme = "wave",
-        background = {
-          dark = "wave",
-          light = "lotus",
-        },
-      }
+          overrides = function(colors)
+            local theme = colors.theme
+            return {
+              -- cmp-nvim
+              Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+              PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+              PmenuSbar = { bg = theme.ui.bg_m1 },
+              PmenuThumb = { bg = theme.ui.bg_p2 },
+              -- transparent floats
+              NormalFloat = { bg = "none" },
+              FloatBorder = { bg = "none" },
+              FloatTitle = { bg = "none" },
+              NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+              LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+              MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            }
+          end,
+          theme = "wave",
+          background = {
+            dark = "wave",
+            light = "lotus",
+          },
+        }
 
-      if colorscheme == "kanagawa" then
         vim.cmd.colorscheme "kanagawa"
 
         -- transparent telescope
@@ -129,9 +128,8 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     config = function()
-      require("catppuccin").setup {}
-
       if colorscheme == "catppuccin" then
+        require("catppuccin").setup {}
         vim.cmd.colorscheme "catppuccin"
       end
     end,
