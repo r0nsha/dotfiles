d1635a6c5638f18cee537e093aca7a95ca36858d
r0nsha <r0nsh4v1t@gmail.com>
2023-09-06 22:18:51 +0300

    indentscope v3

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,33 +1,36 @@
 return {
   {
     "lukas-reineke/indent-blankline.nvim",
+    branch = "v3",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      -- char = "│",
-      char = "┊",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
-    },
-  },
-  {
-    "echasnovski/mini.indentscope",
-    event = { "BufReadPre", "BufNewFile" },
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "toggleterm" },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
-      require("mini.indentscope").setup {
-        symbol = "│",
-        options = { try_as_border = true },
-        draw = {
-          delay = 0,
-          animation = require("mini.indentscope").gen_animation.none(),
+      require("ibl").setup {
+        indent = {
+          char = "┊",
+          highlight = { "Whitespace" },
+        },
+        scope = {
+          char = "│",
+          highlight = { "Normal" },
+        },
+        exclude = {
+          filetypes = {
+            "markdown",
+            "lspinfo",
+            "packer",
+            "checkhealth",
+            "man",
+            "gitcommit",
+            "TelescopePrompt",
+            "TelescopeResults",
+            "help",
+            "alpha",
+            "dashboard",
+            "neo-tree",
+            "Trouble",
+            "lazy",
+          },
         },
       }
     end,
