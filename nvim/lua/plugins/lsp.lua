7a891d1a2e32a510bc3f9342665e4371d6f6bc50
r0nsha <r0nsh4v1t@gmail.com>
2023-12-12 12:03:51 +0200

    disable eslint_d

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -170,10 +170,10 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("lint").linters_by_ft = {
-        javascript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescript = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
+        -- javascript = { "eslint_d" },
+        -- javascriptreact = { "eslint_d" },
+        -- typescript = { "eslint_d" },
+        -- typescriptreact = { "eslint_d" },
       }
 
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
