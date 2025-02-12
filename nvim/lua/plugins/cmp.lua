84a322d28bc2ba579a4325ad0e1c7b45a6dba324
ron <ron@pop-os.local>
2025-02-12 18:35:25 +0200

    complete words from visible buffers

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -73,7 +73,19 @@ return {
           { name = "luasnip" },
           { name = "git" },
           { name = "path" },
-          { name = "buffer", keyword_length = 3 },
+          {
+            name = "buffer",
+            keyword_length = 3,
+            option = {
+              get_bufnrs = function()
+                local bufs = {}
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                  bufs[vim.api.nvim_win_get_buf(win)] = true
+                end
+                return vim.tbl_keys(bufs)
+              end,
+            },
+          },
           -- { name = "cmdline" },
         },
         window = {
