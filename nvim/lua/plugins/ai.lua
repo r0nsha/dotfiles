5a762fca38c366fc0f3392cdf9067f1504c8b267
ron <ron@pop-os.local>
2025-02-13 13:25:55 +0200

    better ai completion keybindings

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -2,7 +2,13 @@ return {
   {
     "supermaven-inc/supermaven-nvim",
     config = function()
-      require("supermaven-nvim").setup {}
+      require("supermaven-nvim").setup {
+        keymaps = {
+          accept_suggestion = "<c-j>",
+          clear_suggestion = "<c-x>",
+          accept_word = "<c-l>",
+        },
+      }
     end,
   },
 }
