44743efabed52b2d8cb3667e76edc1036c84c5bc
r0nsha <r0nsh4v1t@gmail.com>
2025-05-03 14:49:42 +0300

    change theme to yellow

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -48,7 +48,7 @@ if [ "$MACHINE" = "linux" ]; then
 	running "installing GTK theme..."
 	GTK_THEMES=$HOME/.local/share/themes
 	mkdir -p $GTK_THEMES
-	sudo bash -c "$DOTFILES/gtk/Kanagawa-GTK-Theme/themes/install.sh --dest $GTK_THEMES --name Kanagawa --theme default --size compact --tweaks dragon black float"
+	sudo bash -c "$DOTFILES/gtk/Kanagawa-GTK-Theme/themes/install.sh --dest $GTK_THEMES --name Kanagawa --theme yellow --size compact --tweaks dragon black float"
 	success "installed GTK theme"
 
 	running "installing GTK icons..."
