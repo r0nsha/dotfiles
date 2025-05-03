f90c3cf2daf49a78228ef17c844984acbda01d82
r0nsha <r0nsh4v1t@gmail.com>
2025-05-03 13:02:35 +0300

    more vars

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -51,6 +51,8 @@ install_fonts() {
 	local fonts_base_url=https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0
 	local font=Iosevka
 	local font_term=IosevkaTerm
+	local font_zip=$DOWNLOADS/$font.zip
+	local font_term_zip=$DOWNLOADS/$font_term.zip
 
 	# Check if fonts are already installed
 	if [[ $(ls "$fonts" | grep -c "${font// /}NerdFont") -gt 0 &&
@@ -60,13 +62,13 @@ install_fonts() {
 	fi
 
 	running "downloading $font and $font_term from $fonts_base_url..."
-	wget -nv -O $DOWNLOADS/$font.zip $fonts_base_url/$font.zip &
-	wget -nv -O $DOWNLOADS/$font_term.zip $fonts_base_url/$font_term.zip &
+	wget -nv -O $font_zip $fonts_base_url/$font.zip &
+	wget -nv -O $font_term_zip $fonts_base_url/$font_term.zip &
 	wait
 
 	running "installing $font and $font_term in $fonts..."
-	unzip -q -o $DOWNLOADS/$font.zip '*.ttf' -d $fonts &
-	unzip -q -o $DOWNLOADS/$font_term.zip '*.ttf' -d $fonts &
+	unzip -q -o $font_zip '*.ttf' -d $fonts &
+	unzip -q -o $font_term_zip '*.ttf' -d $fonts &
 	wait
 
 	success "fonts installed"
