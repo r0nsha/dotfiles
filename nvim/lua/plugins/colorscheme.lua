fccc8325e57fb94c888c684db0b932653bfc0b44
r0nsha <r0nsh4v1t@gmail.com>
2025-05-18 14:35:52 +0300

    better kanagawa colors

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -35,32 +35,35 @@ return {
         overrides = function(colors)
           local theme = colors.theme
 
+          local makeDiagnosticColor = function(color)
+            local c = require "kanagawa.lib.color"
+            return { fg = color, bg = c(color):blend(theme.ui.bg, 0.975):to_hex() }
+          end
+
           return {
             -- show ~ at end of buffer
             EndOfBuffer = { fg = theme.ui.special },
             NonText = { fg = theme.ui.special },
+
             -- cmp-nvim
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend },
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
             PmenuSel = { fg = "none", bg = theme.ui.bg_p2 },
             PmenuSbar = { bg = theme.ui.bg_m1 },
             PmenuThumb = { bg = theme.ui.bg_p2 },
+
             -- transparent floats
             NormalFloat = { bg = "none" },
             FloatBorder = { bg = "none" },
             FloatTitle = { bg = "none" },
             NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            -- TelescopeBorder = { bg = "none" },
-            -- TelescopeTitle = { bg = "none" },
-            -- block-like telescope ui
-            TelescopeTitle = { fg = theme.ui.special, bold = true },
-            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+            LazyNormal = { bg = theme.ui.bg_m1, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_m1, fg = theme.ui.fg_dim },
+
+            -- diagnostic color tints
+            DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
+            DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
+            DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
+            DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
           }
         end,
         theme = "dragon",
