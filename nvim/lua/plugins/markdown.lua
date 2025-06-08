cf8999ff0f1e1856207a0d64650602f9ad54fa91
r0nsha <r0nsh4v1t@gmail.com>
2025-06-08 18:28:40 +0300

    add checkmate.nvim

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -4,9 +4,39 @@ return {
     event = { "VeryLazy" },
     build = "deno task --quiet build:fast",
     config = function()
-      require("peek").setup()
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+      local peek = require "peek"
+      peek.setup {
+        filetype = { "markdown", "conf" },
+      }
+
+      vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
+      vim.api.nvim_create_user_command("PeekClose", peek.close, {})
+
+      vim.keymap.set("n", "gpp", peek.open, { desc = "Peek: Open" })
+      vim.keymap.set("n", "gpc", peek.close, { desc = "Peek: Close" })
     end,
+  },
+  {
+    "bngarren/checkmate.nvim",
+    ft = "markdown",
+    opts = {
+      files = {
+        "todo",
+        "TODO",
+        "todo.md",
+        "TODO.md",
+        "*.todo",
+        "*.todo.md",
+      },
+      keys = {
+        ["<c-space>"] = "toggle",
+        ["<leader>Tt"] = "toggle",
+        ["<leader>Tc"] = "check",
+        ["<leader>Tu"] = "uncheck",
+        ["<leader>Tn"] = "create",
+        ["<leader>TR"] = "remove_all_metadata",
+        ["<leader>Ta"] = "archive",
+      },
+    },
   },
 }
