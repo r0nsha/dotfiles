271d73b190f4b8d6aa3d75f3d226c0b7ddd51d50
r0nsha <r0nsh4v1t@gmail.com>
2025-06-10 13:16:15 +0300

    disable virtual lines when there are no diagnostics

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -393,15 +393,32 @@ return {
       },
     }
 
+    local function enable_virtual_lines()
+      vim.notify "Virtual lines enabled"
+      vim.diagnostic.config { virtual_text = false, virtual_lines = true }
+    end
+
+    local function disable_virtual_lines()
+      vim.notify "Virtual lines disabled"
+      vim.diagnostic.config { virtual_text = { current_line = true }, virtual_lines = false }
+    end
+
     vim.keymap.set("n", "<leader>l", function()
       local config = vim.diagnostic.config() or {}
       if config.virtual_lines then
-        vim.notify "Virtual lines disabled"
-        vim.diagnostic.config { virtual_text = { current_line = true }, virtual_lines = false }
+        disable_virtual_lines()
       else
-        vim.notify "Virtual lines enabled"
-        vim.diagnostic.config { virtual_text = false, virtual_lines = true }
+        enable_virtual_lines()
       end
     end, { desc = "LSP: Toggle line diagnostics" })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "DiagnosticChanged",
+      callback = function()
+        if vim.diagnostic.count() == 0 then
+          disable_virtual_lines()
+        end
+      end,
+    })
   end,
 }
