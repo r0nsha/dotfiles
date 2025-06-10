5e9427c12e8a74a320f6f4540dcd9c3871cd56f4
r0nsha <r0nsh4v1t@gmail.com>
2025-06-10 14:23:55 +0300

    remove useless comment

--- bin/install/gtk.sh
+++ bin/install/gtk.sh
@@ -5,10 +5,4 @@ if [ "$MACHINE" = "linux" ]; then
 	mkdir -p $GTK_THEMES
 	sudo bash -c "$DOTFILES/gtk/Kanagawa-GTK-Theme/themes/install.sh --dest $GTK_THEMES --name Kanagawa --theme yellow --size compact --tweaks dragon black float"
 	success "installed GTK theme"
-
-	# running "installing GTK icons..."
-	# GTK_ICONS=$HOME/.local/share/icons
-	# mkdir -p $GTK_ICONS
-	# cp -r $DOTFILES/gtk/Kanagawa-GTK-Theme/icons/* $GTK_ICONS/.
-	# success "installed GTK icons"
 fi
