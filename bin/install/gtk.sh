77b3a3ad4870ce17cb9c9a9c6737b6c7083b9621
r0nsha <r0nsh4v1t@gmail.com>
2025-06-10 15:07:50 +0300

    rose-pine gtk

--- bin/install/gtk.sh
+++ bin/install/gtk.sh
@@ -2,10 +2,26 @@
 
 # Install GTK user theme
 if [ "$MACHINE" = "linux" ]; then
+	gtk_themes=$HOME/.local/share/themes
+	gtk_icons=$HOME/.local/share/icons
+
 	running "installing GTK theme..."
-	GTK_THEMES=$HOME/.local/share/themes
-	mkdir -p $GTK_THEMES
-	# sudo bash -c "$DOTFILES/gtk/Kanagawa-GTK-Theme/themes/install.sh --dest $GTK_THEMES --name Kanagawa --theme yellow --size compact --tweaks dragon black float"
-	sudo bash -c "$DOTFILES/gtk/Rose-Pine-GTK-Theme/themes/install.sh --dest $GTK_THEMES --name Rose_Pine --theme default --size standard --tweaks black"
+	mkdir -p $gtk_themes
+	# sudo bash -c "$DOTFILES/gtk/Kanagawa-GTK-Theme/themes/install.sh --dest $gtk_themes --name Kanagawa --theme yellow --size compact --tweaks dragon black float"
+	sudo bash -c "$DOTFILES/gtk/Rose-Pine-GTK-Theme/themes/install.sh --dest $gtk_themes --name Rose-Pine --theme default --size standard --tweaks black"
 	success "installed GTK theme"
+
+	running "installing GTK icons..."
+	mkdir -p $gtk_icons
+	cp -r $DOTFILES/gtk/Rose-Pine-GTK-Theme/icons/* $gtk_icons/.
+	success "installed GTK icons"
+
+	running "installing GTK cursor..."
+	cursor_url=https://github.com/rose-pine/cursor/releases/download/v1.1.0/BreezeX-RosePine-Linux.tar.xz
+	cursor_tar=$DOWNLOADS/Rose-Pine.tar.xz
+	running "downloading GTK cursor from $cursor_url..."
+	wget -nv -O $cursor_tar $cursor_url
+	running "installing $cursor_tar in $gtk_icons..."
+	tar -xvf $cursor_tar -C $gtk_icons
+	success "installed GTK cursor"
 fi
