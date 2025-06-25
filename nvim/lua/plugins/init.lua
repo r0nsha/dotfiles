e253a8afdb62cc88a7b3556d211712c1cef5ee9d
r0nsha <r0nsh4v1t@gmail.com>
2025-06-25 12:34:10 +0300

    remove web-devicons

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,11 +1,5 @@
 return {
   { "nvim-lua/plenary.nvim" },
-  -- {
-  --   "nvim-tree/nvim-web-devicons",
-  --   config = function()
-  --     require("nvim-web-devicons").setup()
-  --   end,
-  -- },
   {
     "folke/lazydev.nvim",
     ft = "lua",
