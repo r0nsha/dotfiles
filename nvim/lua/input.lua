ead18b1172bcf8cbdb4808ea2c69873139a8164c
r0nsha <r0nsh4v1t@gmail.com>
2025-06-26 15:50:47 +0300

    done!

--- nvim/lua/input.lua
+++ nvim/lua/input.lua
@@ -40,15 +40,84 @@ local function get_relative_win_config()
   end
 end
 
----@param s string
----@param len integer
----@return string[]
-local function split_string_by_len(s, len)
-  local subs = {}
-  for i = 1, #s, len do
-    table.insert(subs, s:sub(i, i + len - 1))
+local function estimate_wrapped_lines(text, width)
+  local breakat = vim.o.breakat
+  local linebreak = vim.wo.linebreak
+  local showbreak = vim.o.showbreak
+  local showbreak_width = vim.fn.strdisplaywidth(showbreak)
+
+  local result = {}
+  local current_line = ""
+  local current_width = 0
+  local last_break_idx = nil
+
+  local i = 1
+  while i <= #text do
+    local char = text:sub(i, i)
+
+    local char_width = vim.fn.strdisplaywidth(char)
+
+    -- Track legal break point
+    if breakat:find(vim.pesc(char), 1, true) then
+      last_break_idx = #current_line + 1
+    end
+
+    -- If this char would overflow screen width
+    if current_width + char_width > width then
+      if linebreak and last_break_idx then
+        table.insert(result, current_line:sub(1, last_break_idx))
+        i = i - (#current_line - last_break_idx)
+      elseif last_break_idx then
+        table.insert(result, current_line:sub(1, last_break_idx))
+        i = i - (#current_line - last_break_idx)
+      else
+        table.insert(result, current_line)
+      end
+
+      -- Reset for wrapped line
+      current_line = showbreak
+      current_width = showbreak_width
+      last_break_idx = nil
+    else
+      current_line = current_line .. char
+      current_width = current_width + char_width
+      i = i + 1
+    end
   end
-  return subs
+
+  if #current_line > 0 then
+    table.insert(result, current_line)
+  end
+
+  return result
+end
+
+---@param winnr integer
+---@param bufnr integer
+---@param config input.Config
+local function resize(winnr, bufnr, config)
+  local line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1]
+
+  if line == "" then
+    vim.api.nvim_win_set_width(winnr, config.padding)
+    vim.api.nvim_win_set_height(winnr, 1)
+    return
+  end
+
+  local lines = estimate_wrapped_lines(line, config.max_width)
+
+  -- set width
+  local lens = vim.tbl_map(function(l)
+    return vim.fn.strdisplaywidth(l)
+  end, lines)
+  local width = math.max(unpack(lens)) + config.padding
+  width = width > config.max_width and config.max_width or width
+  vim.api.nvim_win_set_width(winnr, width + 1)
+
+  -- set height
+  local height = #lines
+  height = height > config.max_height and config.max_height or height
+  vim.api.nvim_win_set_height(winnr, height)
 end
 
 ---@param winnr integer
@@ -59,84 +128,73 @@ local function setup_autocmds(winnr, bufnr, config)
     group = augroup,
     buffer = bufnr,
     callback = function()
-      local line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1]
-
-      if line == "" then
-        vim.api.nvim_win_set_width(winnr, config.padding)
-        vim.api.nvim_win_set_height(winnr, 1)
-        return
-      end
-
-      local lines = split_string_by_len(line, config.max_width)
-      local lens = vim.tbl_map(function(l)
-        return vim.fn.strdisplaywidth(l)
-      end, lines)
-
-      local width = math.max(unpack(lens)) + config.padding
-      width = width > config.max_width and config.max_width or width
-      vim.api.nvim_win_set_width(winnr, width + 1)
-
-      local height = #lines
-      height = height > config.max_height and config.max_height or height
-      vim.api.nvim_win_set_height(winnr, height)
+      resize(winnr, bufnr, config)
     end,
   })
+end
+
+---@param options table<string, any>
+---@param opts vim.api.keyset.option
+local function set_options(options, opts)
+  for k, v in pairs(options) do
+    vim.api.nvim_set_option_value(k, v, opts)
+  end
+end
+
+---@param winnr integer
+---@param bufnr integer
+---@param on_confirm fun(input?: string)
+local function setup_mappings(winnr, bufnr, on_confirm)
+  ---@param result string?
+  local function close(result)
+    vim.cmd "stopinsert"
+    vim.api.nvim_win_close(winnr, true)
+    on_confirm(result)
+  end
+
+  ---@param mode string|string[]
+  ---@param lhs string
+  ---@param rhs string|function
+  local function map(mode, lhs, rhs)
+    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr })
+  end
+
+  map({ "n", "i", "v" }, "<cr>", function()
+    close(vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1])
+  end)
+  map("n", "<esc>", close)
+  map("n", "q", close)
 end
 
 ---@param config input.Config
 ---@param on_confirm fun(input: string?)
 function M.input(config, on_confirm)
-  config = vim.tbl_deep_extend("force", defaults, config)
-
-  -- Calculate a minimal width with a bit of buffer
-  local default_width = vim.str_utfindex(config.default, "utf-8") + config.padding
-  local prompt_width = vim.str_utfindex(config.prompt, "utf-8") + config.padding
-  local width = default_width > prompt_width and default_width or prompt_width
-  width = width > config.max_width and config.max_width or width
-
-  local relative_win_config = get_relative_win_config()
-  config = vim.tbl_deep_extend("keep", config, { win = relative_win_config }, { win = { width = width } })
-
+  config = vim.tbl_deep_extend("force", defaults, config, { win = { title = config.prompt } })
   on_confirm = on_confirm or function() end
+
+  config = vim.tbl_deep_extend(
+    "keep",
+    config,
+    { win = get_relative_win_config() },
+    { win = { width = vim.fn.strdisplaywidth(config.default) + config.padding } }
+  )
 
   -- Create buffer and floating window.
   local bufnr = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_set_option_value("buftype", "prompt", { buf = bufnr })
-  vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = bufnr })
+  set_options({ buftype = "prompt", bufhidden = "wipe", textwidth = config.max_width }, { buf = bufnr })
   vim.fn.prompt_setprompt(bufnr, "")
 
   local winnr = vim.api.nvim_open_win(bufnr, true, config.win)
-  vim.api.nvim_set_option_value("wrap", true, { win = winnr })
-  vim.api.nvim_set_option_value("linebreak", true, { win = winnr })
+  set_options({ wrap = true, linebreak = true, winhighlight = "Search:None" }, { win = winnr })
 
-  -- Write default value
+  -- write default value and put cursor at the end
   vim.api.nvim_buf_set_text(bufnr, 0, 0, 0, 0, { config.default })
-
-  -- Put cursor at the end of the default value
   vim.cmd "startinsert"
   vim.api.nvim_win_set_cursor(winnr, { 1, vim.str_utfindex(config.default, "utf-8") + 1 })
 
-  -- Enter to confirm
-  vim.keymap.set({ "n", "i", "v" }, "<cr>", function()
-    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)
-    vim.cmd "stopinsert"
-    vim.api.nvim_win_close(winnr, true)
-    on_confirm(lines[1])
-  end, { buffer = bufnr })
+  resize(winnr, bufnr, config)
 
-  -- Esc or q to close
-  vim.keymap.set("n", "<esc>", function()
-    vim.cmd "stopinsert"
-    vim.api.nvim_win_close(winnr, true)
-    on_confirm(nil)
-  end, { buffer = bufnr })
-
-  vim.keymap.set("n", "q", function()
-    vim.cmd "stopinsert"
-    vim.api.nvim_win_close(winnr, true)
-    on_confirm(nil)
-  end, { buffer = bufnr })
-
+  setup_mappings(winnr, bufnr, on_confirm)
   setup_autocmds(winnr, bufnr, config)
 end
 
