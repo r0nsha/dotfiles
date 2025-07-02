e392f1eb0833c4c0a05ea3a7fa8d1b4fafa8dfe1
r0nsha <r0nsh4v1t@gmail.com>
2025-07-02 13:20:23 +0300

    fish: change up configuration

--- fish/config.fish
+++ fish/config.fish
@@ -6,35 +6,26 @@ source $DOTFILES/fish/functions.fish
 source $DOTFILES/fish/aliases.fish
 source $DOTFILES/fish/vars.fish
 source $DOTFILES/fish/macos.fish
-fish_config theme choose "Rosé Pine"
 
-# Vi mode
+stty -ixon # disable C-s and C-q
+
+set fish_greeting # disable welcome message
 fish_vi_key_bindings
 
-# Disable welcome message
-set fish_greeting
+# theme
+fish_config theme choose "Rosé Pine"
 
-# Emulates vim's cursor shape behavior
-# Set the normal and visual mode cursors to a block
+# cursor
 set fish_cursor_default block
-# Set the insert mode cursor to a line
 set fish_cursor_insert block
-# Set the replace mode cursor to an underscore
 set fish_cursor_replace_one block
-# The following variable can be used to configure cursor shape in
-# visual mode, but due to fish_cursor_default, is redundant here
 set fish_cursor_visual block
 
+# paths
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
-
-# npm
 fish_add_path ./node_modules/.bin
-
-# Node stuff
 set -Ux NVM_DIR "$HOME/.nvm"
-
-# pnpm
 set -Ux PNPM_HOME "$HOME/.local/share/pnpm"
 fish_add_path $PNPM_HOME
 
