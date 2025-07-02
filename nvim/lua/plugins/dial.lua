f282575d46d9c431871a731cfd98ac919596c9f3
r0nsha <r0nsh4v1t@gmail.com>
2025-07-02 14:11:43 +0300

    fix dial.nvim mappings

--- nvim/lua/plugins/dial.lua
+++ nvim/lua/plugins/dial.lua
@@ -1,18 +1,49 @@
 return {
   "monaqa/dial.nvim",
-  event = "VeryLazy",
   config = function()
+    local augend = require "dial.augend"
+    require("dial.config").augends:register_group {
+      default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias["%d/%m/%Y"],
+      },
+      visual = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias["%d/%m/%Y"],
+        augend.constant.alias.alpha,
+        augend.constant.alias.Alpha,
+      },
+    }
+
     local map = require "dial.map"
-    vim.keymap.set("n", "+", map.inc_normal, { desc = "Increment" })
-    vim.keymap.set("n", "-", map.dec_normal, { desc = "Decrement" })
+    vim.keymap.set("n", "+", function()
+      return map.inc_normal()
+    end, { expr = true, desc = "Increment" })
+    vim.keymap.set("n", "-", function()
+      return map.dec_normal()
+    end, { expr = true, desc = "Decrement" })
 
-    vim.keymap.set("n", "g+", map.inc_gnormal, { desc = "Increment" })
-    vim.keymap.set("n", "g-", map.dec_gnormal, { desc = "Decrement" })
+    vim.keymap.set("n", "g+", function()
+      return map.inc_gnormal()
+    end, { expr = true, desc = "Increment" })
+    vim.keymap.set("n", "g-", function()
+      return map.dec_gnormal()
+    end, { expr = true, desc = "Decrement" })
 
-    vim.keymap.set("v", "+", map.inc_visual, { desc = "Increment" })
-    vim.keymap.set("v", "-", map.dec_visual, { desc = "Decrement" })
+    vim.keymap.set("v", "+", function()
+      return map.inc_visual()
+    end, { expr = true, desc = "Increment" })
+    vim.keymap.set("v", "-", function()
+      return map.dec_visual()
+    end, { expr = true, desc = "Decrement" })
 
-    vim.keymap.set("v", "g+", map.inc_gvisual, { desc = "Increment" })
-    vim.keymap.set("v", "g-", map.dec_gvisual, { desc = "Decrement" })
+    vim.keymap.set("v", "g+", function()
+      return map.inc_gvisual()
+    end, { expr = true, desc = "Increment" })
+    vim.keymap.set("v", "g-", function()
+      return map.dec_gvisual()
+    end, { expr = true, desc = "Decrement" })
   end,
 }
