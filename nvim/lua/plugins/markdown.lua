208b66115d2f09c493bccdaac7114d7c5b8926ed
r0nsha <r0nsh4v1t@gmail.com>
2025-07-03 17:38:31 +0300

    configure render-markdown

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -2,6 +2,16 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = { "markdown", "codecompanion" },
+    opts = {
+      checkbox = {
+        checked = { icon = " ", highlight = "RenderMarkdownChecked", scope_highlight = nil },
+        unchecked = { icon = " ", highlight = "RenderMarkdownUnchecked", scope_highlight = nil },
+        custom = {
+          in_progress = { raw = "[-]", rendered = " ", highlight = "RenderMarkdownBullet", scope_highlight = nil },
+          cancelled = { raw = "[/]", rendered = "󱋭 ", highlight = "RenderMarkdownError", scope_highlight = nil },
+        },
+      },
+    },
   },
   {
     "toppair/peek.nvim",
