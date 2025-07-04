682940c2fc65aa65d5a64b20ef16328f3a80fec8
r0nsha <r0nsh4v1t@gmail.com>
2025-07-04 11:51:15 +0300

    remove peek.nvim

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -13,22 +13,5 @@ return {
       },
     },
   },
-  {
-    "toppair/peek.nvim",
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    config = function()
-      local peek = require("peek")
-      peek.setup({
-        filetype = { "markdown", "conf" },
-      })
-
-      vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
-      vim.api.nvim_create_user_command("PeekClose", peek.close, {})
-
-      vim.keymap.set("n", "gpp", peek.open, { desc = "Peek: Open" })
-      vim.keymap.set("n", "gpc", peek.close, { desc = "Peek: Close" })
-    end,
-  },
   { "jghauser/follow-md-links.nvim", ft = "markdown" },
 }
