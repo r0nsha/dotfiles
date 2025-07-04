b168b7ecba6a14175725b9d8edef3d0e09e9522e
r0nsha <r0nsh4v1t@gmail.com>
2025-07-04 16:59:11 +0300

    wip

--- bin/install2.sh
+++ bin/install2.sh
@@ -2,10 +2,7 @@
 #
 # install.sh installs my things.
 
-set -e
-set -o pipefail
-set -u
-set -x
+set -euo pipefail
 
 script_dir=$(dirname "$0")
 DOTFILES=$(realpath -s $script_dir/..)
@@ -25,46 +22,104 @@ Darwin) MACHINE="darwin" ;;
 esac
 
 if [ "$MACHINE" = "unknown" ]; then
-    echo "Error: Unsupported operating system. Exiting." >&2
-    exit 1
+    error "unsupported operating system: $(uname)"
 fi
 
 cd $DOTFILES
-mkdir -p $LOCAL_BIN $LOCAL_SHARE $DOWNLOADS        # create required directories
-echo "set -Ux DOTFILES $DOTFILES" >$HOME/.env.fish # create local env file
-chmod ug+x $DOTFILES/hooks/*                       # make git hooks executable
 
-# init git submodules
+mkdir -p $LOCAL_BIN $LOCAL_SHARE $DOWNLOADS
+
+LOCAL_ENV=$HOME/.env.fish
+if [ ! -f "$LOCAL_ENV" ]; then
+    step "creating local env file"
+    echo "set -Ux DOTFILES $DOTFILES" >$HOME/.env.fish
+    success
+fi
+
+step "git"
+chmod ug+x $DOTFILES/hooks/*
 git submodule init
 git submodule update --init --recursive
+success
 
+step "wallpapers"
 ln -sf $DOTFILES/wallpapers $HOME/Pictures/Wallpapers # link wallpapers
+success
 
 # load dconf settings
 if exists "dconf"; then
+    step "dconf"
     dconf load / <$DOTFILES/dconf/settings.ini
+    success
 fi
 
 # load gtk theme
-
 if [ "$MACHINE" = "linux" ]; then
-    GTK_THEMES="$LOCAL_SHARE/themes"
-    GTK_ICONS="$LOCAL_SHARE/icons"
+    step "gtk theme"
+    GTK_THEMES=$LOCAL_SHARE/themes
+    GTK_ICONS=$LOCAL_SHARE/icons
 
-    # Create directories, similar to os.makedirs(..., exist_ok=True)
-    mkdir -p "$GTK_THEMES"
-    mkdir -p "$GTK_ICONS"
+    mkdir -p $GTK_THEMES $GTK_ICONS
 
-    # commands(
-    #     [
-    #         f'sudo bash -c "{env.dirs.dotfiles}/gtk/Rose-Pine-GTK-Theme/themes/install.sh --dest {gtk_themes} --name Rose-Pine --theme default --size compact --tweaks black"',
-    #         f"cp -r {env.dirs.dotfiles}/gtk/Rose-Pine-GTK-Theme/icons {gtk_icons}",
-    #     ]
-    # )
+    sudo $DOTFILES/gtk/Rose-Pine-GTK-Theme/themes/install.sh --dest $GTK_THEMES --name Rose-Pine --theme default --size compact --tweaks black
+    cp -r $DOTFILES/gtk/Rose-Pine-GTK-Theme/icons $GTK_ICONS
+    success
 fi
 
+# install fonts
+case "$MACHINE" in
+linux)
+    FONTS_DIR=$LOCAL_SHARE/fonts
+    ;;
+darwin)
+    FONTS_DIR=$HOME/Library/Fonts
+    ;;
+esac
+
+mkdir -p $FONTS_DIR
+FONTS_BASE_URL=https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0
+
+not_installed_fonts() {
+    local files=$(ls $FONTS_DIR)
+    local not_installed=()
+    for font in "$@"; do
+        local pattern=$(echo "$font" | tr -d ' ')"NerdFont"
+        if echo "$files" | grep -q "$pattern"; then
+            not_installed+=("$font")
+        fi
+    done
+    echo "${not_installed[@]}"
+}
+
+FONTS_TO_INSTALL=("Iosevka" "IosevkaTerm")
+FONTS_TO_INSTALL=$(not_installed_fonts $FONTS_TO_INSTALL)
+if [ -n "$FONTS_TO_INSTALL" ]; then
+    step "installing fonts"
+
+    FONT_URLS=()
+    for f in "${FONTS_TO_INSTALL[@]}"; do
+        FONT_URLS+=("$FONTS_BASE_URL/${f}.zip")
+    done
+
+    for url in "${FONT_URLS[@]}"; do
+        curl -#L "$url" -o "$DOWNLOADS/$(basename $url)"
+    done
+
+    wait
+
+    # printf "%s\n" "${FONT_URLS[@]}" | xargs -P $(nproc) -I {} curl -#Lv {} #-o "$DOWNLOADS/$(basename {})"
+    # # printf "%s\n" "${FONT_URLS[@]}" | xargs -P $(nproc) -I {} echo "$DOWNLOADS/$(basename {})"
+
+    # unzip -o "$DOWNLOADS/*.zip" -d $FONTS_DIR >/dev/null
+
+    success
+fi
+
+# macos defaults
 if [ "$MACHINE" = "darwin" ]; then
+    step "setting macos defaults"
     source $DOTFILES/bin/macos_defaults.sh
+    success
 fi
 
 echo "i installed your things :)"
--- bin/utils.sh
+++ bin/utils.sh
@@ -1,26 +1,39 @@
-BLUE="\033[0;34m"
-GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
+GREEN="\033[0;32m"
 RED="\033[0;31m"
 NC="\033[0m"
 
-info() {
-	echo "${BLUE}•${NC} $1"
+CURR_STEP=""
+trap '[ -n "$CURR_STEP" ] && error' EXIT
+
+print_header() {
+    local term_width
+    term_width=$(tput cols)
+    local header_text="$1 "
+    local header_len=${#header_text}
+    local num_dashes=$((term_width - header_len))
+    local dashes=""
+    for ((i = 0; i < num_dashes; i++)); do
+        dashes+="-"
+    done
+    echo "${header_text}${dashes}"
+}
+
+step() {
+    CURR_STEP="$1"
+    echo -e "${YELLOW}$(print_header "• $CURR_STEP")${NC}"
 }
 
 success() {
-	echo "${GREEN}✔${NC} $1"
-}
-
-warn() {
-	echo "${YELLOW}⚠${NC} $1"
+    echo -e "${GREEN}$(print_header "✔ $CURR_STEP")${NC}\n"
+    CURR_STEP=""
 }
 
 error() {
-	echo "${RED}✘${NC} $1" >&2
-	exit 1
+    echo -e "${RED}$(print_header "✘ $CURR_STEP")${NC}" >&2
+    exit 1
 }
 
 exists() {
-	command -v "$1" >/dev/null 2>&1
+    command -v "$1" >/dev/null 2>&1
 }
