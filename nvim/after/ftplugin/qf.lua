7ff5888ae805646a718267988440c5de6eb09384
r0nsha <r0nsh4v1t@gmail.com>
2025-07-07 01:00:35 +0300

    remap <cr> in qflist

---
+++ nvim/after/ftplugin/qf.lua
@@ -0,0 +1,13 @@
+vim.keymap.set("n", "<CR>", function()
+  local qflist = vim.fn.getqflist()
+  local item = qflist[vim.fn.line(".")]
+
+  if not item then
+    return
+  end
+
+  vim.cmd("cclose")
+
+  vim.cmd("e " .. vim.api.nvim_buf_get_name(item.bufnr))
+  vim.fn.cursor(item.lnum, item.col)
+end, { buffer = 0, silent = true })
