d6ae670d64910bb3b2c10a6f2cd1a5b90d8bb8dc
r0nsha <r0nsh4v1t@gmail.com>
2025-07-08 00:41:20 +0300

    comment out fidget.nvim

--- nvim/lua/plugins/fidget.lua
+++ nvim/lua/plugins/fidget.lua
@@ -1,27 +1,26 @@
 return {
-  "j-hui/fidget.nvim",
-  enabled = false,
-  config = function()
-    require("fidget").setup({
-      notification = {
-        override_vim_notify = true,
-        configs = {
-          default = vim.tbl_deep_extend("force", require("fidget.notification").default_config, {
-            icon = "",
-          }),
-        },
-        view = {
-          group_separator = " ",
-        },
-        window = {
-          winblend = 0,
-          x_padding = 1,
-          y_padding = 0,
-          border = "single",
-          align = "bottom",
-          relative = "editor",
-        },
-      },
-    })
-  end,
+  -- "j-hui/fidget.nvim",
+  -- config = function()
+  --   require("fidget").setup({
+  --     notification = {
+  --       override_vim_notify = true,
+  --       configs = {
+  --         default = vim.tbl_deep_extend("force", require("fidget.notification").default_config, {
+  --           icon = "",
+  --         }),
+  --       },
+  --       view = {
+  --         group_separator = " ",
+  --       },
+  --       window = {
+  --         winblend = 0,
+  --         x_padding = 1,
+  --         y_padding = 0,
+  --         border = "single",
+  --         align = "bottom",
+  --         relative = "editor",
+  --       },
+  --     },
+  --   })
+  -- end,
 }
