40951a43e2c0092d5b538a7e51e90cbbf4b6864a
r0nsha <r0nsh4v1t@gmail.com>
2025-07-09 01:19:49 +0300

    try disabling autopair plugins

--- nvim/lua/plugins/pair.lua
+++ nvim/lua/plugins/pair.lua
@@ -1,22 +1,22 @@
 return {
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {},
-    config = true,
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    event = "InsertEnter",
-    config = function()
-      require("nvim-ts-autotag").setup({
-        opts = {
-          enable_close = true,
-          enable_rename = true,
-          enable_close_on_slash = true,
-        },
-      })
-    end,
-  },
-  { "RRethy/nvim-treesitter-endwise" },
+  -- {
+  --   "windwp/nvim-autopairs",
+  --   event = "InsertEnter",
+  --   opts = {},
+  --   config = true,
+  -- },
+  -- {
+  --   "windwp/nvim-ts-autotag",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("nvim-ts-autotag").setup({
+  --       opts = {
+  --         enable_close = true,
+  --         enable_rename = true,
+  --         enable_close_on_slash = true,
+  --       },
+  --     })
+  --   end,
+  -- },
+  -- { "RRethy/nvim-treesitter-endwise" },
 }
