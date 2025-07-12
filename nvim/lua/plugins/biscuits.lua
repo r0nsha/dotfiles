0c58bf3c313ebe2630b1f3ab4ef0123f17e8066d
r0nsha <r0nsh4v1t@gmail.com>
2025-07-12 12:45:27 +0300

    add nvim-biscuits

---
+++ nvim/lua/plugins/biscuits.lua
@@ -0,0 +1,11 @@
+return {
+  "code-biscuits/nvim-biscuits",
+  config = function()
+    require("nvim-biscuits").setup({
+      cursor_line_only = true,
+      prefix_string = " ",
+    })
+
+    vim.api.nvim_set_hl(0, "BiscuitColor", { link = "Comment" })
+  end,
+}
