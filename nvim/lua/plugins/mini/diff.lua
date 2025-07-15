a4620311f02f467210be306656df2b2394118250
r0nsha <r0nsh4v1t@gmail.com>
2025-07-15 21:15:28 +0300

    add mini.diff mappings

--- nvim/lua/plugins/mini/diff.lua
+++ nvim/lua/plugins/mini/diff.lua
@@ -4,6 +4,15 @@ return {
     local diff = require("mini.diff")
     diff.setup({
       source = diff.gen_source.none(),
+      mappings = {
+        apply = "gh",
+        reset = "gH",
+        textobject = "gh",
+        goto_first = "[H",
+        goto_prev = "[h",
+        goto_next = "]h",
+        goto_last = "]H",
+      },
     })
   end,
 }
