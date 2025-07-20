0fc5472d93e2835af976ab06f6325d7a9c0b11e6
r0nsha <r0nsh4v1t@gmail.com>
2025-07-20 17:32:17 +0300

    move pair commented code to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -118,4 +118,24 @@ return {
       vim.keymap.set("n", "<leader>cn", "<cmd>TailwindFoldToggle<cr>", { desc = "Tailwind Fold: Toggle" })
     end,
   },
+  -- {
+  --   "windwp/nvim-autopairs",
+  --   event = "InsertEnter",
+  --   opts = {},
+  --   config = true,
+  -- },
+  -- {
+  --   "windwp/nvim-ts-autotag",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("nvim-ts-autotag").setup({
+  --       opts = {
+  --         enable_close = true,
+  --         enable_rename = true,
+  --         enable_close_on_slash = true,
+  --       },
+  --     })
+  --   end,
+  -- },
+  -- { "RRethy/nvim-treesitter-endwise" },
 }
