b783860580e93804bccb706b852422c2191f4404
r0nsha <r0nsh4v1t@gmail.com>
2025-07-21 20:28:22 +0300

    set resolution!

--- i3/scripts/set_resolution.fish
+++ i3/scripts/set_resolution.fish
@@ -1,7 +1,3 @@
 #!/usr/bin/env fish
 set -l dp (xrandr --listactivemonitors | awk '{print $NF}' | tail -n 1)
-set -l mode "5120x1440_120.00"
-# xrandr --newmode "$mode" 1324.26 5120 5560 6136 7152 1440 1441 1444 1543 -HSync +VSync
-# xrandr --newmode "$mode" 1324.26 5120 5560 6136 7152 1440 1441 1444 1543 -HSync +VSync
-# xrandr --addmode $dp "$mode"
-# xrandr --output $dp --mode "$mode" --rate 120
+xrandr --output $dp --size 5120x1440 --rate 120.00
