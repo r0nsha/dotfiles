e36e7d3acd36e4bfcd2be0a6f0d68e9f8ece0cd4
r0nsha <r0nsh4v1t@gmail.com>
2025-07-23 04:16:44 +0300

    wip

--- i3/scripts/system_options.fish
+++ i3/scripts/system_options.fish
@@ -1,7 +1,5 @@
 #!/usr/bin/env fish
 xss-lock --transfer-sleep-lock -- betterlockscreen -l
-sleep 5
-dunstify hello
-# xset r rate 250 30
-# xrandr --size 5120x1440
-# xrandr --output DP-4 --size 5120x1440 --rate 120.00
+xset r rate 250 30
+xrandr --size 5120x1440
+xrandr --output DP-4 --size 5120x1440 --rate 120.00
