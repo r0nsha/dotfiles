e02d480ee19c86cb3bc1296b57c18b9ab5a0568a
r0nsha <r0nsh4v1t@gmail.com>
2025-07-24 20:07:34 +0300

    wip

--- i3blocks/scripts/lang.fish
+++ i3blocks/scripts/lang.fish
@@ -1,12 +1,8 @@
 #!/usr/bin/env fish
 
-# Set defaults if not already set
-set -q font; or set font monospace
-set -q font_weight; or set font_weight bold
-
 while true
     # Output current layout
-    xkb-switch -p | awk -v font="$font" -v font_weight="$font_weight" '{print toupper($0)}' || sleep 1
+    xkb-switch -p | awk '{print toupper($0)}' || sleep 1
 
     # Wait for layout change
     xkb-switch -w
