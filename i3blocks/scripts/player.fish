df531415718692cca6e2f3950c3f89036f67450d
r0nsha <r0nsh4v1t@gmail.com>
2025-07-25 15:57:31 +0300

    i3blocks player script

--- i3blocks/scripts/player.fish
+++ i3blocks/scripts/player.fish
@@ -1,4 +1,37 @@
 #!/usr/bin/env fish
 
 # 󰈹󰓇
-playerctl metadata --follow --format ' 󰎇 {{ artist }} - {{ title }} - {{duration(position)}} '
+
+set -l player (playerctl metadata --format '{{playerName}}')
+
+if test -z "$player"; or test "$player" = "No player found"
+    exit
+end
+
+set -l url (playerctl metadata --format '{{xesam:url}}')
+if string match -q "*youtube*" "$url"
+    set player_icon "󰗃"
+else if string match -q "*vimeo*" "$url"
+    set player_icon "󰕷"
+else if test "$player" = spotify
+    set player_icon "󰓇"
+else if test "$player" = vlc
+    set player_icon "󰕼"
+else if test "$player" = firefox
+    set player_icon "󰈹"
+else if test "$player" = chrome
+    set player_icon "󰊯"
+else
+    set player_icon "󰎇"
+end
+
+# set -l _status (playerctl status)
+# if test "$_status" = Playing
+#     set _status "󰐊"
+# else
+#     set _status "󰏤"
+# end
+
+set -l song (playerctl metadata --format '{{trunc(artist, 20)}} - {{trunc(title, 30)}} {{duration(position)}}/{{duration(mpris:length)}}')
+
+echo " $player_icon $song "
