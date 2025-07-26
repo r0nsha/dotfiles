3d8b72f7814030cb9c83ccd821addb0d39c551af
r0nsha <r0nsh4v1t@gmail.com>
2025-07-26 19:21:23 +0300

    remove swap usage

--- i3blocks/scripts/resources.fish
+++ i3blocks/scripts/resources.fish
@@ -2,5 +2,4 @@
 
 set cpu_usage (top -bn1 | rg "Cpu\(s\)" | awk '{ if ($2 == 0.0) { print "0" } else { printf "%.1f\n", $2 } }')
 set ram_usage (free -m | awk 'NR==2{printf "%.1f", $3*100/$2 }')
-set swap_usage (free -m | awk 'NR==3{printf "%.1f", $3*100/$2 }')
-echo " 󰓅 $cpu_usage%  $ram_usage%  $swap_usage% "
+echo " 󰓅 $cpu_usage%  $ram_usage% "
