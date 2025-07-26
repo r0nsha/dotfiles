c1c909c147c786e2cc6ed2646bb9f757beba72a8
r0nsha <r0nsh4v1t@gmail.com>
2025-07-26 19:52:16 +0300

    udiskie!

---
+++ rofi/scripts/rofi-usb.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+# A rofi menu for selecting udisks devices using udiskie
+
+set -l options (
+    for device in (udiskie-info -a)
+        set -l label (udiskie-info $device -o "󰕓 {device_file} | {drive_label} {ui_id_uuid}")
+        notify-send (udiskie-info $device -o "{is_mounted}")
+        if test (udiskie-info $device -o "{is_mounted}") = True
+            set label "$label (Mounted)"
+        end
+        echo $label
+    end | sort | string join "\n"
+)
+set -l picked (echo -e $options | rofi -dmenu -p "USB Devices" -theme-str "window {width: 20%;}")
+set -l device (echo $picked | awk '{print $2}')
+
+if test -z "$device"
+    exit
+end
+
+if mount | rg -q "$device"
+    notify-send "Unmounting $device"
+    udiskie-unmount "$device"
+else
+    notify-send "Mounting $device"
+    udiskie-mount "$device"
+end
