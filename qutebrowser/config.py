92c538bebe0d08165f5d6d5b17638c73863de142
r0nsha <r0nsh4v1t@gmail.com>
2025-07-27 14:00:06 +0300

    qutebrowser stuff

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,5 +1,19 @@
 config.load_autoconfig(False)
 
+# Options
+c.auto_save.session = True
+c.content.blocking.method = "adblock"
+c.content.blocking.adblock.lists = [
+    "https://easylist.to/easylist/easylist.txt",
+    "https://easylist.to/easylist/easyprivacy.txt",
+    "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
+    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+    "https://secure.fanboy.co.nz/fanboy-social.txt",
+]
+
+# Keybindings
+config.bind("<Ctrl-r>", "config-source ;; message-info 'Reloaded config'")
+
 # Spoof User-Agent for Google domains
 google_domains = ["https://accounts.google.com/*", "https://*.google.com/*"]
 for domain in google_domains:
