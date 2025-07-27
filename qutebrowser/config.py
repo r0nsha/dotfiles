0d30ceed4e3af5ab75cd0c7650a987c1bb241f74
r0nsha <r0nsh4v1t@gmail.com>
2025-07-27 15:48:32 +0300

    privacy stuff

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -18,6 +18,19 @@ c.completion.use_best_match = True
 c.hints.mode = "letter"
 c.hints.min_chars = 1
 c.content.pdfjs = True
+c.content.notifications.enabled = False
+c.content.tls.certificate_errors = "ask-block-thirdparty"
+
+# Adblock
+c.content.blocking.enabled = True
+c.content.blocking.method = "both"
+c.content.blocking.adblock.lists = [
+    "https://easylist.to/easylist/easylist.txt",
+    "https://easylist.to/easylist/easyprivacy.txt",
+    "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
+    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+    "https://secure.fanboy.co.nz/fanboy-social.txt",
+]
 
 # Search engines
 c.url.searchengines["DEFAULT"] = "https://duckduckgo.com/?q={}"
@@ -34,40 +47,27 @@ c.url.searchengines["gh"] = "https://github.com/search?q={}&type=Code"
 # Aliases
 c.aliases["jellyfin"] = "open -t http://localhost:8096"
 
-# Theme
-modus_vivendi = {
-    "bg": "#000000",
-    "bg_dim": "#303030",
-    "fg": "#ffffff",
-    "fg_dim": "#989898",
-    "red": "#ff5f59",
-    "green": "#44bc44",
-    "yellow": "#d0bc00",
-    "cyan": "#00d3d0",
-}
-
-colors = modus_vivendi
-
-c.colors.tabs.selected.odd.bg = colors["bg"]
-c.colors.tabs.selected.even.bg = colors["bg"]
-c.colors.tabs.selected.odd.fg = colors["fg"]
-c.colors.tabs.selected.even.fg = colors["fg"]
-c.colors.tabs.bar.bg = colors["bg_dim"]
-c.colors.tabs.indicator.error = colors["red"]
-c.colors.tabs.indicator.start = colors["green"]
-c.colors.tabs.indicator.stop = colors["green"]
+# Colors
+c.colors.tabs.selected.odd.bg = "black"
+c.colors.tabs.selected.even.bg = "black"
+c.colors.tabs.selected.odd.fg = "white"
+c.colors.tabs.selected.even.fg = "white"
+c.colors.tabs.bar.bg = "darkslategrey"
+c.colors.tabs.indicator.error = "red"
+c.colors.tabs.indicator.start = "green"
+c.colors.tabs.indicator.stop = "green"
 c.colors.tabs.indicator.system = "none"
-c.colors.tabs.odd.bg = colors["bg_dim"]
-c.colors.tabs.even.bg = colors["bg_dim"]
-c.colors.tabs.odd.fg = colors["fg"]
-c.colors.tabs.even.fg = colors["fg"]
-c.colors.tabs.pinned.odd.bg = colors["cyan"]
-c.colors.tabs.pinned.even.bg = colors["cyan"]
-c.colors.tabs.pinned.odd.fg = colors["fg"]
-c.colors.tabs.pinned.even.fg = colors["fg"]
-c.colors.tooltip.bg = colors["bg"]
-c.colors.tooltip.fg = colors["fg"]
-c.colors.webpage.bg = colors["bg"]
+c.colors.tabs.odd.bg = "darkslategrey"
+c.colors.tabs.even.bg = "darkslategrey"
+c.colors.tabs.odd.fg = "white"
+c.colors.tabs.even.fg = "white"
+c.colors.tabs.pinned.odd.bg = "cyan"
+c.colors.tabs.pinned.even.bg = "cyan"
+c.colors.tabs.pinned.odd.fg = "white"
+c.colors.tabs.pinned.even.fg = "white"
+c.colors.tooltip.bg = "black"
+c.colors.tooltip.fg = "white"
+c.colors.webpage.bg = "black"
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.algorithm = "lightness-cielab"
 c.colors.webpage.darkmode.contrast = 0.0
@@ -76,17 +76,10 @@ c.colors.webpage.darkmode.policy.images = "smart"
 c.colors.webpage.darkmode.threshold.background = 128
 c.colors.webpage.darkmode.threshold.foreground = 128
 c.colors.webpage.preferred_color_scheme = "dark"
-c.content.notifications.enabled = False
-c.content.tls.certificate_errors = "ask-block-thirdparty"
-c.content.blocking.enabled = True
-c.content.blocking.method = "both"
-c.content.blocking.adblock.lists = [
-    "https://easylist.to/easylist/easylist.txt",
-    "https://easylist.to/easylist/easyprivacy.txt",
-    "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
-    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
-    "https://secure.fanboy.co.nz/fanboy-social.txt",
-]
+c.colors.statusbar.private.bg = "darkred"
+c.colors.statusbar.private.fg = "white"
+c.colors.statusbar.command.private.bg = "red"
+c.colors.statusbar.command.private.fg = "white"
 
 # Keybindings
 config.bind("J", "tab-prev")
