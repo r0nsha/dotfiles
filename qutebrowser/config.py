44e03931dfafbd2043b3b1d6a8f46ae09da9a286
r0nsha <r0nsh4v1t@gmail.com>
2025-07-27 13:22:49 +0300

    wip: qutebrowser config

---
+++ qutebrowser/config.py
@@ -0,0 +1,8 @@
+# Spoof User-Agent for Google domains
+google_domains = ["https://accounts.google.com/*", "https://*.google.com/*"]
+for domain in google_domains:
+    config.set(
+        "content.headers.user_agent",
+        "Mozilla/5.0 ({os_info}; rv:130.0) Gecko/20100101 Firefox/130",
+        domain,
+    )
