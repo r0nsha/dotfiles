fdddbbce59fa3681e780129bef6b4d8712ace0d5
r0nsha <r0nsh4v1t@gmail.com>
2025-08-01 17:39:16 +0300

    autotype!

--- rofi/scripts/rofi-pass.fish
+++ rofi/scripts/rofi-pass.fish
@@ -1,8 +1,6 @@
 #!/usr/bin/env fish
 
 # TODO: autotype (email/user > tab > pass)
-# TODO: pass...
-# TODO: copy with ctrl-c
 # TODO: show last
 # TODO: insert new pass
 
@@ -30,6 +28,17 @@ function copy_to_clipboard
     end
 end
 
+function type_text
+    set -l text $argv[1]
+    set -l delay 0
+    switch $type_backend
+        case xdotool
+            xdotool type --delay $delay --clearmodifiers $text
+        case wtype
+            wtype -d $delay $text
+    end
+end
+
 function list_all
     set -l passwords (fd .gpg $pass_dir | sed -E "s|$pass_dir\/(.*)\.gpg|\\1|" | sort | string join "\n")
     set -l picked (
@@ -49,9 +58,9 @@ function list_all
 
     switch $rofi_exit
         case 0 # enter
-            dunstify enter
+            show_autotype_options $picked
         case 10 # alt-o
-            show $picked
+            show_password $picked
         case 11 # alt-p
             copy_target $picked pass
         case 12 # alt-e
@@ -61,10 +70,46 @@ function list_all
     end
 end
 
-function show
+function show_autotype_options
+    set -l password $argv[1]
+    set -l contents (pass show $password | string join "\n")
+    set -l options "󰅁 Return\nautotype\npass\n$(echo -e $contents | tail -n +2 | awk -F ':' '{print $1}')"
+    set -l picked (echo -e $options | rofi -dmenu -p $password -mesg "enter Type | alt-c Copy to clipboard" -kb-custom-1 "alt-c")
+    set -l rofi_exit $status
+
+    if test -z "$picked"
+        return
+    end
+
+    if string match -q "*Return" $picked
+        list_all
+        return
+    end
+
+    if test "$picked" = autotype
+        notify-send autotype
+        return
+    end
+
+    if test "$picked" = pass
+        set picked_contents (echo -e $contents | head -n1)
+    else
+        set picked_contents (echo -e $contents | rg "^$picked" | awk -F ': ' '{print $2}')
+    end
+
+    switch $rofi_exit
+        case 0 # enter
+            type_text $picked_contents
+        case 10 # alt-c
+            copy_to_clipboard $picked_contents
+            notify-send "Copied $picked to clipboard"
+    end
+end
+
+function show_password
     set -l password $argv[1]
     set -l options "󰅁 Return\npass: $(pass show $password)"
-    set -l picked (echo -e $options | rofi -dmenu -p $password)
+    set -l picked (echo -e $options | rofi -dmenu -p $password -mesg "Press enter to copy to clipboard")
 
     if test -z "$picked"
         return
@@ -85,7 +130,6 @@ function copy_target
     set -l target $argv[2]
     set -l contents (pass show $password | string join "\n")
 
-    # find $target in $contents
     if test "$target" = pass
         set target_contents (echo -e $contents | head -n1)
     else
