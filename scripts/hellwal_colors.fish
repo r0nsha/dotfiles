bf29a2c50d9313b7fcfddab96d3f9a5ec3ee913b
r0nsha <r0nsh4v1t@gmail.com>
2025-08-10 18:25:15 +0300

    wip

---
+++ scripts/hellwal_colors.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+
+pkill -SIGUSR2 waybar
+
+ln -sf ~/.cache/hellwal/wired.ron ~/.config/wired.ron
+
+mkdir -p ~/.config/qt5ct/colors
+ln -sf ~/.cache/hellwal/qt.conf ~/.config/qt5ct/colors/colors.conf
+
+mkdir -p ~/.config/qt6ct/colors
+ln -sf ~/.cache/hellwal/qt.conf ~/.config/qt6ct/colors/colors.conf
