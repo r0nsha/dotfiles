04f61345df89099dbf8100a191c6c72380bbfd1a
r0nsha <r0nsh4v1t@gmail.com>
2025-08-11 00:12:19 +0300

    done: hellwal colors in kitty

--- scripts/hellwal_colors.fish
+++ scripts/hellwal_colors.fish
@@ -8,5 +8,6 @@ ln -sf ~/.cache/hellwal/qt.conf ~/.config/qt5ct/colors/colors.conf
 mkdir -p ~/.config/qt6ct/colors
 ln -sf ~/.cache/hellwal/qt.conf ~/.config/qt6ct/colors/colors.conf
 
+pkill -SIGUSR1 kitty
 pkill -SIGUSR2 waybar
 qutebrowser :config-source
