af92f89b5c9b38caf959ef84b12c37298d62789f
r0nsha <r0nsh4v1t@gmail.com>
2025-08-14 17:28:31 +0300

    crop instead of fit

--- scripts/swww_set.fish
+++ scripts/swww_set.fish
@@ -3,7 +3,7 @@
 
 # set background
 set img $argv[1]
-swww img --resize=fit "$img" --transition-fps=120 --transition-step=4 --transition-type=random
+swww img --resize=crop "$img" --transition-fps=120 --transition-step=4 --transition-type=random
 
 # link to cache so that other apps like the lockscreen can reference it
 ln -sf "$img" ~/.cache/swww/.current_background
