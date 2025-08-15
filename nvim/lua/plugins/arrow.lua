5ff541ee22f7e8ecf8e309c6a83d6a00b918b07e
r0nsha <r0nsh4v1t@gmail.com>
2025-08-15 16:21:45 +0300

    snipe -> arrow

---
+++ nvim/lua/plugins/arrow.lua
@@ -0,0 +1,31 @@
+return {
+  "otavioschwanck/arrow.nvim",
+  dependencies = { "echasnovski/mini.icons" },
+  opts = {
+    show_icons = true,
+    leader_key = "'",
+    buffer_leader_key = ";",
+    mappings = {
+      edit = "e",
+      delete_mode = "d",
+      clear_all_items = "C",
+      toggle = "a", -- used as save if separate_save_and_remove is true
+      open_vertical = "v",
+      open_horizontal = "_",
+      quit = "q",
+      remove = "x", -- only used if separate_save_and_remove is true
+      next_item = "]",
+      prev_item = "[",
+    },
+    window = {
+      relative = "editor",
+      anchor = "SW",
+      row = vim.o.lines - 2,
+      col = vim.o.columns,
+      border = "single",
+      zindex = 999,
+    },
+    save_key = "git_root",
+    index_keys = "sfghjkl'zcbwrtyuiopnm,./123456789ASDFGHJKLZXVBQWERTYUIOPNM",
+  },
+}
--- nvim/lua/plugins/snipe.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "leath-dub/snipe.nvim",
-  config = function()
-    local snipe = require("snipe")
-    snipe.setup({})
-
-    vim.keymap.set("n", "'", snipe.open_buffer_menu, { desc = "Snipe" })
-  end,
-}
