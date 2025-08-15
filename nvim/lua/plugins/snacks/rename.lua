0ba3e8aecbcc14938e425fc643b8b6f421752662
r0nsha <r0nsh4v1t@gmail.com>
2025-08-15 14:57:33 +0300

    fix snacks rename

--- nvim/lua/plugins/snacks/rename.lua
+++ nvim/lua/plugins/snacks/rename.lua
@@ -1,17 +1,22 @@
+vim.api.nvim_create_autocmd("User", {
+  pattern = "OilActionsPost",
+  callback = function(event)
+    if event.data.actions.type == "move" then
+      Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+    end
+  end,
+})
+
 return {
   "folke/snacks.nvim",
-  dependencies = { "stevearc/oil.nvim" },
   opts = {},
-  config = function()
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "OilActionsPost",
-      callback = function(event)
-        if event.data.actions.type == "move" then
-          Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
-        end
+  keys = {
+    {
+      "grN",
+      function()
+        Snacks.rename.rename_file()
       end,
-    })
-
-    vim.keymap.set("n", "grN", function() end, { desc = "Rename current file" })
-  end,
+      desc = "Rename current file",
+    },
+  },
 }
