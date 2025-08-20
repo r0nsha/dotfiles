f196e481d5c3feb41d8457e2682989b9eea33c01
r0nsha <r0nsh4v1t@gmail.com>
2025-08-20 14:07:15 +0300

    fix theme switcher for tmux

--- scripts/wal.fish
+++ scripts/wal.fish
@@ -1,8 +1,26 @@
 #!/usr/bin/env fish
 
+function read_theme
+    if test -f ~/.cache/theme
+        set theme (cat ~/.cache/theme)
+    else
+        echo $THEME
+    end
+
+    switch $theme
+        case dark
+            echo dark
+        case light
+            echo light
+        case '*'
+            echo dark
+    end
+end
+
+set theme (read_theme)
 set hellwal_args
 
-if test "$THEME" = light
+if test "$theme" = light
     set -a hellwal_args --light
 else
     set -a hellwal_args --dark
