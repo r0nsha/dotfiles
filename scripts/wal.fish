c75f137870845147b4aa495e8cdf3b4507f294d6
r0nsha <r0nsh4v1t@gmail.com>
2025-08-23 14:47:26 +0300

    wip

--- scripts/wal.fish
+++ scripts/wal.fish
@@ -24,7 +24,6 @@ if test "$theme" = light
     set -a hellwal_args --light
 else
     set -a hellwal_args --dark
-    set -a hellwal_args --bright-offset 0.25
 end
 
 hellwal -q -i ~/.cache/swww/.current_background $hellwal_args --check-contrast --script "~/.config/scripts/color_my_system.fish"
