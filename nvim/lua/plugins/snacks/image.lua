bc79920bd3a6a447dedfdd97c0b4c300fef8738a
r0nsha <r0nsh4v1t@gmail.com>
2025-09-13 20:46:58 +0300

    snacks image thing

--- nvim/lua/plugins/snacks/image.lua
+++ nvim/lua/plugins/snacks/image.lua
@@ -2,13 +2,25 @@ return {
   "folke/snacks.nvim",
   opts = {
     image = {
-      doc = {},
+      doc = {
+        inline = false,
+        float = false,
+      },
     },
     styles = {
       snacks_image = {
         border = "single",
         col = 0,
       },
+    },
+  },
+  keys = {
+    {
+      "<leader>ci",
+      function()
+        require("snacks").image.hover()
+      end,
+      desc = "Image: Hover",
     },
   },
 }
