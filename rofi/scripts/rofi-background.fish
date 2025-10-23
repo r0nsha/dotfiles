66d2c33983b0e94f8abca37ff97dad0e0b79ca36
r0nsha <r0nsh4v1t@gmail.com>
2025-10-23 13:43:39 +0300

    rofi-background: preview images

--- rofi/scripts/rofi-background.fish
+++ rofi/scripts/rofi-background.fish
@@ -5,7 +5,10 @@ if test -z "$dir"
     set dir ~/pictures/backgrounds
 end
 
-set picked (fd . ~/pictures/backgrounds | xargs -I% basename % | rofi -dmenu -p "background")
+set picked (for bg in (fd . ~/pictures/backgrounds)
+    set -l name (basename $bg)
+    echo -en "$name\0icon\x1f$bg\n"
+end | rofi -dmenu -theme fullscreen-preview.rasi)
 
 if test -z "$picked"
     exit
