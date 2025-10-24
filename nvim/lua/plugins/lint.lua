024434ba8898f7149c51a597dd658afb7a936cd7
r0nsha <r0nsh4v1t@gmail.com>
2025-10-25 02:18:07 +0300

    reenable eslint_d

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -5,10 +5,10 @@ return {
     require("lint").linters_by_ft = {
       fish = { "fish" },
       python = { "ruff" },
-      -- javascript = { "eslint_d" },
-      -- javascriptreact = { "eslint_d" },
-      -- typescript = { "eslint_d" },
-      -- typescriptreact = { "eslint_d" },
+      javascript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescript = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
     }
 
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
