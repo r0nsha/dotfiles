21f0caa5c3fd42b0db990b20304dd1fce3163454
r0nsha <r0nsh4v1t@gmail.com>
2025-10-25 00:16:00 +0300

    remove suda.vim

--- nvim/lua/plugins/suda.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "lambdalisue/suda.vim",
-  keys = {
-    { "<leader>w", "<cmd>w<cr>", desc = "Write the current file" },
-    { "<leader>W", "<cmd>SudaWrite<cr>", desc = "Write the current file with sudo" },
-  },
-  cmd = { "SudaWrite", "SudaRead" },
-}
