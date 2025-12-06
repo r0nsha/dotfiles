ceb9fc6f9d74fab6847e8b31382bb34deb337bf3
r0nsha <r0nsh4v1t@gmail.com>
2025-12-06 02:31:32 +0200

    configure multicursor.nvim

--- nvim/lua/plugins/multicursor.lua
+++ nvim/lua/plugins/multicursor.lua
@@ -12,57 +12,62 @@ return {
     end
 
     -- Add or skip cursor above/below the main cursor.
-    vim.keymap.set({ "n", "x" }, "<M-c>", function()
+    vim.keymap.set({ "n", "x" }, "<Up>", function()
       mc.lineAddCursor(-1)
     end, opts("Add Cursor Above"))
-    vim.keymap.set({ "n", "x" }, "C", function()
+    vim.keymap.set({ "n", "x" }, "<Down>", function()
       mc.lineAddCursor(1)
     end, opts("Add Cursor Below"))
 
     -- Add a new cursor by matching word/selection
-    vim.keymap.set({ "n", "x" }, "<c-N>", function()
+    vim.keymap.set({ "n", "x" }, "<C-N>", function()
       mc.matchAddCursor(-1)
     end, opts("Add Cursor Match Above"))
-    vim.keymap.set({ "n", "x" }, "<c-n>", function()
+    vim.keymap.set({ "n", "x" }, "<C-n>", function()
       mc.matchAddCursor(1)
     end, opts("Add Cursor Match Below"))
 
-    -- -- Press `mWi"ap` will create a cursor in every match of string captured by `i"` inside range `ap`.
-    vim.keymap.set("n", "mw", mc.operator, opts("Operator"))
+    -- Easy way to add and remove cursors using the main cursor.
+    vim.keymap.set("n", "<C-m>", mc.toggleCursor, opts("Toggle Cursor"))
 
     -- Add all matches in the document
-    vim.keymap.set("n", "mm", mc.matchAllAddCursors, opts("Add Cursors to All Matches"))
+    vim.keymap.set("n", "<C-M>", mc.matchAllAddCursors, opts("Add Cursors to All Matches"))
 
     -- Match new cursors within visual selections by regex.
-    vim.keymap.set("x", "mm", mc.matchCursors, opts("Match Cursors by Regex"))
+    vim.keymap.set("x", "m", mc.matchCursors, opts("Match Cursors by Regex"))
 
-    -- Easy way to add and remove cursors using the main cursor.
-    vim.keymap.set({ "n", "x" }, "mt", mc.toggleCursor, opts("Toggle Cursor"))
+    -- Split visual selections by regex.
+    vim.keymap.set("x", "M", mc.splitCursors, opts("Split Cursors by Regex"))
+
+    -- Bring back cursors if you accidentally clear them
+    vim.keymap.set("n", "gM", mc.restoreCursors, opts("Restore Cursors"))
+
+    -- Append/insert for each line of visual selections.
+    vim.keymap.set("x", "I", mc.insertVisual, opts("Insert"))
+    vim.keymap.set("x", "A", mc.appendVisual, opts("Append"))
+
+    -- Jumplist support
+    vim.keymap.set({ "n", "x" }, "<C-i>", mc.jumpForward, opts("Jump Forward"))
+    vim.keymap.set({ "n", "x" }, "<C-o>", mc.jumpBackward, opts("Jump Backward"))
 
     mc.addKeymapLayer(function(layerSet)
-      layerSet({ "n", "x" }, "<M-Q>", function()
+      layerSet({ "n", "x" }, "<C-Up>", function()
         mc.lineSkipCursor(-1)
       end, { desc = "Skip Cursor Above" })
-      layerSet({ "n", "x" }, "<M-q>", function()
+      layerSet({ "n", "x" }, "<C-Down>", function()
         mc.lineSkipCursor(1)
       end, { desc = "Skip Cursor Below" })
 
-      layerSet({ "n", "x" }, "<c-Q>", function()
+      layerSet({ "n", "x" }, "<C-Q>", function()
         mc.matchSkipCursor(-1)
       end, { desc = "Skip Cursor Match Above" })
-      layerSet({ "n", "x" }, "<c-q>", function()
+      layerSet({ "n", "x" }, "<C-q>", function()
         mc.matchSkipCursor(1)
       end, { desc = "Skip Cursor Match Below" })
 
       -- Rotate the main cursor.
-      layerSet({ "n", "x" }, "<left>", mc.prevCursor, { desc = "Rotate Cursor Left" })
-      layerSet({ "n", "x" }, "<right>", mc.nextCursor, { desc = "Rotate Cursor Right" })
-
-      -- Delete the main cursor.
-      layerSet({ "n", "x" }, "mx", mc.deleteCursor, { desc = "Delete Cursor" })
-
-      -- Clone every cursor and disable the originals.
-      layerSet({ "n", "x" }, "md", mc.duplicateCursors, { desc = "Duplicate Cursors" })
+      layerSet({ "n", "x" }, "<Left>", mc.prevCursor, { desc = "Rotate Cursor Left" })
+      layerSet({ "n", "x" }, "<Right>", mc.nextCursor, { desc = "Rotate Cursor Right" })
 
       layerSet("n", "<esc>", function()
         if not mc.cursorsEnabled() then
@@ -73,23 +78,6 @@ return {
           -- Default <esc> handler.
         end
       end)
-
-      -- Align cursor columns.
-      layerSet("n", "ma", mc.alignCursors, { desc = "Align Cursors" })
-
-      -- Bring back cursors if you accidentally clear them
-      layerSet("n", "mr", mc.restoreCursors, { desc = "Restore Cursors" })
     end)
-
-    -- Split visual selections by regex.
-    vim.keymap.set("x", "ms", mc.splitCursors, opts("Split Cursors by Regex"))
-
-    -- Append/insert for each line of visual selections.
-    vim.keymap.set("x", "I", mc.insertVisual, opts("Insert"))
-    vim.keymap.set("x", "A", mc.appendVisual, opts("Append"))
-
-    -- Jumplist support
-    vim.keymap.set({ "x", "n" }, "<c-i>", mc.jumpForward, opts("Jump Forward"))
-    vim.keymap.set({ "x", "n" }, "<c-o>", mc.jumpBackward, opts("Jump Backward"))
   end,
 }
--- nvim/lua/plugins/vcs/jj.lua
+++ nvim/lua/plugins/vcs/jj.lua
@@ -7,9 +7,9 @@ return {
       jj.setup({})
 
       local cmd = require("jj.cmd")
-      vim.keymap.set("n", "<leader>gg", cmd.status, { desc = "JJ status" })
+      vim.keymap.set("n", "<leader>gg", cmd.log, { desc = "JJ log" })
+      vim.keymap.set("n", "<leader>gs", cmd.status, { desc = "JJ status" })
       vim.keymap.set("n", "<leader>gD", cmd.diff, { desc = "JJ diff" })
-      vim.keymap.set("n", "<leader>gl", cmd.log, { desc = "JJ log" })
     end,
   },
 }
