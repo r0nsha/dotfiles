feef574ddb5a0af8e3bc53dffe3698361a17bf77
r0nsha <r0nsh4v1t@gmail.com>
2025-12-13 16:09:09 +0200

    remove wezterm from dotfiles

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,17 +4,17 @@ return {
     "folke/lazydev.nvim",
     ft = "lua",
     dependencies = {
-      {
-        "DrKJeff16/wezterm-types",
-        version = false,
-        lazy = true,
-        name = "wezterm-types",
-      },
+      -- {
+      --   "DrKJeff16/wezterm-types",
+      --   version = false,
+      --   lazy = true,
+      --   name = "wezterm-types",
+      -- },
     },
     opts = {
       library = {
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        { path = "wezterm-types", mods = { "wezterm" } },
+        -- { path = "wezterm-types", mods = { "wezterm" } },
       },
     },
   },
--- wezterm/wezterm.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require("wezterm") ---@type Wezterm
-local act = wezterm.action
-
-local config = wezterm.config_builder() ---@type Config
-
-config.max_fps = 120
-
-return config
