05f477472eeb2d051df6dace1bb5fdda64bfa7fb
Ron Shavit <ron.s@layerxsecurity.com>
2025-12-15 09:32:13 +0200

    changes
    Signed-off-by: Ron Shavit <ron.s@layerxsecurity.com>

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -21,8 +21,9 @@ return {
     config = function()
       require "plugins.lsp.servers"
       require "plugins.lsp.attach"
-      require "plugins.lsp.diagnostic"
       require "plugins.lsp.progress"
+      -- Replaced with tiny-inline-diagnostic.nvim
+      -- require "plugins.lsp.diagnostic"
     end,
   },
   {
@@ -37,5 +38,44 @@ return {
         },
       },
     },
+  },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require("tiny-inline-diagnostic").setup {
+        preset = "minimal",
+        transparent_bg = true,
+        transparent_cursorline = false,
+        options = {
+          show_source = { enabled = true },
+          use_icons_from_diagnostic = false,
+          multilines = {
+            enabled = true,
+            always_show = false,
+            trim_whitespaces = true,
+          },
+          show_all_diags_on_cursorline = false,
+        },
+      }
+
+      -- Disable Neovim's default virtual text diagnostics
+      vim.diagnostic.config { virtual_text = false }
+
+      local icons = require "config.icons"
+      vim.diagnostic.config {
+        virtual_text = false,
+        virtual_lines = false,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.HINT] = icons.hint,
+            [vim.diagnostic.severity.INFO] = icons.info,
+            [vim.diagnostic.severity.WARN] = icons.warning,
+            [vim.diagnostic.severity.ERROR] = icons.error,
+          },
+        },
+      }
+    end,
   },
 }
