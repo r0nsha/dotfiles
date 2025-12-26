8ec56583f6cafc57e9c2178f4290b0ec79a34e3b
r0nsha <r0nsh4v1t@gmail.com>
2025-12-26 23:48:26 +0200

    add lua exec keymaps
    Signed-off-by: r0nsha <r0nsh4v1t@gmail.com>

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,2 +1,4 @@
 -- Execute current buffer
-vim.keymap.set("n", "X", "<cmd>so<cr>", { desc = "Execute buffer", buffer = 0 })
+vim.keymap.set("n", "X", "<cmd>.lua<cr>", { desc = "Execute line", buffer = 0 })
+vim.keymap.set("x", "X", "<cmd>'<,'>lua<cr>", { desc = "Execute visual selection", buffer = 0 })
+vim.keymap.set("n", "<leader>X", "<cmd>so<cr>", { desc = "Execute buffer", buffer = 0 })
