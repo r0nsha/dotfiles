4ee0aa3efb6f89642bd044f26925cec25deaf736
r0nsha <r0nsh4v1t@gmail.com>
2026-01-03 02:30:08 +0200

    disable matchparen
    Signed-off-by: r0nsha <r0nsh4v1t@gmail.com>

--- nvim/lua/config/opts.lua
+++ nvim/lua/config/opts.lua
@@ -5,6 +5,9 @@ vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
 vim.g.loaded_matchparen = 1
 
+-- disable matchparen
+vim.g.loaded_matchparen = 1
+
 -- basic
 vim.opt.exrc = true
 vim.opt.secure = true
