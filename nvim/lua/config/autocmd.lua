9d5e74e29151769dcdbcdd839289a48b634cbb2b
r0nsha <r0nsh4v1t@gmail.com>
2026-01-31 16:32:53 +0200

    change

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -61,8 +61,9 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 vim.api.nvim_create_autocmd("CursorMoved", {
   group = augroup,
   callback = function()
-    if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
-      vim.schedule(function() vim.cmd.nohlsearch() end)
+    if vim.v.hlsearch == 1 then
+      local ok, sc = pcall(vim.fn.searchcount)
+      if ok and sc.exact_match == 0 then vim.schedule(function() vim.cmd.nohlsearch() end) end
     end
   end,
 })
