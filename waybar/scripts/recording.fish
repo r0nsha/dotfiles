b24d833882b99fb0d7fa56dd0138144893bc0f27
r0nsha <r0nsh4v1t@gmail.com>
2026-02-15 20:44:39 +0200

    fix: recording module probably leading to "too many open files"
    Signed-off-by: r0nsha <r0nsh4v1t@gmail.com>

--- waybar/scripts/recording.fish
+++ waybar/scripts/recording.fish
@@ -1,43 +1,21 @@
 #!/usr/bin/env fish
 
-set recorder wf-recorder
+set -l recorder wf-recorder
+set -l pid (pgrep -n "$recorder")
 
-function recorder_pids
-    ps -ef | rg screen.fish | awk '{print $2}' | xargs -I {} pgrep -P {} $recorder
+if test -z "$pid"
+    exit 0
 end
 
-while true
-    echo ""
+set -l elapsed (ps -p "$pid" -o etimes=)
+set -l h (math "floor($elapsed / 3600)")
+set -l m (math "floor($elapsed % 3600 / 60)")
+set -l s (math "floor($elapsed % 3600 % 60)")
 
-    set pids ""
-    while true
-        set -g pids "$(recorder_pids)"
-        if test -n "$pids"
-            break
-        end
-        sleep 1
-    end
-
-    set -l start_time (ps -p $pids[1] -o lstart=)
-    set -l start_epoch (date -d "$start_time" +%s)
-
-    while test -n "$(recorder_pids)"
-        set -l now_epoch (date +%s)
-        set -l elapsed (math "$now_epoch - $start_epoch")
-        set -l h (math "floor($elapsed / 3600)")
-        set -l m (math "floor($elapsed % 3600 / 60)")
-        set -l s (math "floor($elapsed % 3600 % 60)")
-
-        if test "$h" -gt 0
-            set time (printf "%d:%02d:%02d" $h $m $s)
-        else
-            set time (printf "%02d:%02d" $m $s)
-        end
-
-        echo "󰻃 $time"
-
-        sleep 1
-    end
-
-    pidwait $recorder
+if test "$h" -gt 0
+    set time (printf "%d:%02d:%02d" $h $m $s)
+else
+    set time (printf "%02d:%02d" $m $s)
 end
+
+echo "󰻃 $time"
