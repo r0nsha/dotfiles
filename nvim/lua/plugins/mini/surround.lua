9b132fb35ae8402776d8989497c599a537a6fa45
r0nsha <r0nsh4v1t@gmail.com>
2026-04-16 02:40:00 +0300

    nvim/surround: use helix's mappings
    Signed-off-by: r0nsha <r0nsh4v1t@gmail.com>

--- nvim/lua/plugins/mini/surround.lua
+++ nvim/lua/plugins/mini/surround.lua
@@ -1,12 +1,12 @@
 require("mini.surround").setup({
   mappings = {
-    add = "sa",
-    delete = "sd",
-    find = "sf",
-    find_left = "sF",
-    highlight = "sh",
-    replace = "sc",
-    update_n_lines = "sn",
+    add = "ms",
+    delete = "md",
+    find = "",
+    find_left = "",
+    highlight = "",
+    replace = "mr",
+    update_n_lines = "",
   },
   n_lines = 200,
 })
