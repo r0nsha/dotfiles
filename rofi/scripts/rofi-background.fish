da257c7185ea0d9c386431f2855641c626ff6821
r0nsha <r0nsh4v1t@gmail.com>
2026-04-16 01:32:21 +0300

    rofi: preselect current background
    Signed-off-by: r0nsha <r0nsh4v1t@gmail.com>

--- rofi/scripts/rofi-background.fish
+++ rofi/scripts/rofi-background.fish
@@ -1,17 +1,21 @@
 #!/usr/bin/env fish
 
 set dir $argv[1]
-if test -z "$dir"
-    set dir ~/pictures/backgrounds
+set -q dir[1] || set dir ~/pictures/backgrounds
+
+set current_bg (basename (readlink ~/.cache/awww/.current_background 2>/dev/null) 2>/dev/null)
+
+set -l items (fd . $dir)
+set -l selected_idx 0
+
+# Find selected index
+for i in (seq (count $items))
+    test (basename $items[$i]) = "$current_bg" && set selected_idx (math $i - 1)
 end
 
-set picked (for bg in (fd . ~/pictures/backgrounds)
-    set -l name (basename $bg)
-    echo -en "$name\0icon\x1f$bg\n"
-end | rofi -dmenu -theme fullscreen-preview.rasi)
+set picked (for item in $items
+    printf "%s\0icon\x1f%s\n" (basename $item) $item
+end | rofi -dmenu -theme fullscreen-preview.rasi -selected-row $selected_idx)
 
-if test -z "$picked"
-    exit
-end
-
+test -z "$picked" && exit
 ~/.config/scripts/set_background.fish $dir/$picked
