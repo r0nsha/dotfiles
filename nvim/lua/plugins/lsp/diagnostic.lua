7c699c82220c0fd4ab129adf0ada63550761a903
Ron Shavit <me@ronshavit.com>
2026-05-26 22:57:50 +0300

    nvim: remove prioritized diagnostic jump
    Signed-off-by: Ron Shavit <me@ronshavit.com>

--- nvim/lua/plugins/lsp/diagnostic.lua
+++ nvim/lua/plugins/lsp/diagnostic.lua
@@ -1,3 +1,18 @@
+local icons = require("config.icons")
+
+vim.diagnostic.config({
+  virtual_text = false,
+  virtual_lines = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.HINT] = icons.hint,
+      [vim.diagnostic.severity.INFO] = icons.info,
+      [vim.diagnostic.severity.WARN] = icons.warning,
+      [vim.diagnostic.severity.ERROR] = icons.error,
+    },
+  },
+})
+
 require("tiny-inline-diagnostic").setup({
   preset = "minimal",
   transparent_bg = true,
@@ -12,59 +27,6 @@ require("tiny-inline-diagnostic").setup({
     },
     show_all_diags_on_cursorline = false,
   },
-})
-
-local severity_order = {
-  vim.diagnostic.severity.ERROR,
-  vim.diagnostic.severity.WARN,
-  vim.diagnostic.severity.INFO,
-  vim.diagnostic.severity.HINT,
-}
-
-local function get_highest_severity()
-  local count = vim.diagnostic.count(0)
-
-  for _, s in ipairs(severity_order) do
-    if count[s] and count[s] > 0 then return s end
-  end
-
-  return nil
-end
-
-local function jump_prev()
-  local severity = get_highest_severity()
-  vim.diagnostic.jump({ count = vim.v.count1, severity = severity })
-  return severity
-end
-
-local function jump_next()
-  local severity = get_highest_severity()
-  vim.diagnostic.jump({ count = -vim.v.count1, severity = severity })
-  return severity
-end
-
-vim.keymap.set({ "n", "x" }, "[d", jump_prev, {
-  desc = "Jump to the previous diagnostic (prioritized)",
-})
-
-vim.keymap.set({ "n", "x" }, "gj", function()
-  if jump_prev() == nil then
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("gj", true, true, true), "n", true)
-  end
-end, {
-  desc = "Jump to the previous diagnostic (prioritized)",
-})
-
-vim.keymap.set({ "n", "x" }, "]d", jump_next, {
-  desc = "Jump to the next diagnostic (prioritized)",
-})
-
-vim.keymap.set({ "n", "x" }, "gk", function()
-  if jump_next() == nil then
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("gk", true, true, true), "n", true)
-  end
-end, {
-  desc = "Jump to the next diagnostic (prioritized)",
 })
 
 local qf_severity = {
@@ -99,19 +61,6 @@ vim.keymap.set("n", "grQ", function()
 end, { desc = "Show Diagnostics (Filtered)" })
 vim.keymap.set("n", "gre", vim.diagnostic.open_float, { desc = "Open Diagnostic Float" })
 
--- local icons = require("config.icons")
--- vim.diagnostic.config({
---   virtual_text = false,
---   virtual_lines = false,
---   signs = {
---     text = {
---       [vim.diagnostic.severity.HINT] = icons.hint,
---       [vim.diagnostic.severity.INFO] = icons.info,
---       [vim.diagnostic.severity.WARN] = icons.warning,
---       [vim.diagnostic.severity.ERROR] = icons.error,
---     },
---   },
--- })
 -- local icons = require("config.icons")
 
 -- vim.diagnostic.config({
