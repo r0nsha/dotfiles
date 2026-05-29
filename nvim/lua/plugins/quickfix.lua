74ee5e183155c2439cae94c66551b29f2f7b6c87
Ron Shavit <me@ronshavit.com>
2026-05-29 11:54:44 +0300

    nvim/quickfix: add refresh key
    Signed-off-by: Ron Shavit <me@ronshavit.com>

--- nvim/lua/plugins/quickfix.lua
+++ nvim/lua/plugins/quickfix.lua
@@ -1,34 +1,29 @@
-vim.api.nvim_create_autocmd("FileType", {
-  group = require("augroup"),
-  once = true,
-  callback = function()
-    local icons = require("config.icons")
-    local quicker = require("quicker")
+local icons = require("config.icons")
+local quicker = require("quicker")
 
-    quicker.setup({
-      type_icons = {
-        E = icons.error,
-        W = icons.warn,
-        I = icons.info,
-        N = icons.info,
-        H = icons.hint,
-      },
-      keys = {
-        {
-          "<Tab>",
-          function() quicker.expand({ before = 2, after = 2, add_to_existing = true }) end,
-          desc = "Expand quickfix context",
-        },
-        { "<S-Tab>", quicker.collapse, desc = "Collapse quickfix context" },
-      },
-    })
-
-    vim.keymap.set("n", "<leader>q", quicker.toggle, { desc = "Quickfix" })
-    vim.keymap.set(
-      "n",
-      "<leader>Q",
-      function() quicker.toggle({ loclist = true }) end,
-      { desc = "Loclist" }
-    )
-  end,
+quicker.setup({
+  type_icons = {
+    E = icons.error,
+    W = icons.warn,
+    I = icons.info,
+    N = icons.info,
+    H = icons.hint,
+  },
+  keys = {
+    {
+      "<Tab>",
+      function() quicker.expand({ before = 2, after = 2, add_to_existing = true }) end,
+      desc = "Expand quickfix context",
+    },
+    { "<S-Tab>", quicker.collapse, desc = "Collapse quickfix context" },
+    { "<leader>r", quicker.refresh, desc = "Refresh quickfix list" },
+  },
 })
+
+vim.keymap.set("n", "<leader>q", quicker.toggle, { desc = "Quickfix" })
+vim.keymap.set(
+  "n",
+  "<leader>Q",
+  function() quicker.toggle({ loclist = true }) end,
+  { desc = "Loclist" }
+)
