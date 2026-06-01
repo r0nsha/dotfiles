11593b5a0f676255c51afccee24d354267c2f115
Ron Shavit <me@ronshavit.com>
2026-06-01 18:09:01 +0300

    nvim: remove lua ftplugin
    Signed-off-by: Ron Shavit <me@ronshavit.com>

--- nvim/after/ftplugin/lua.lua
+++
@@ -1,3 +0,0 @@
--- Execute current buffer
-vim.keymap.set("n", "<C-x>", "<cmd>.lua<cr>", { desc = "Execute line", buffer = 0 })
-vim.keymap.set("x", "<C-x>", "<cmd>'<,'>lua<cr>", { desc = "Execute visual selection", buffer = 0 })
