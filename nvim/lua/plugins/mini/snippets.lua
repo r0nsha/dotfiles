9410cea5f78f0024cd5d02094ea2adef1ed96e2d
Ron Shavit <me@ronshavit.com>
2026-06-03 10:52:41 +0300

    nvim/snippets: stop mini.snippets when leaving insert mode
    Signed-off-by: Ron Shavit <me@ronshavit.com>

--- nvim/lua/plugins/mini/snippets.lua
+++ nvim/lua/plugins/mini/snippets.lua
@@ -1,11 +1,17 @@
 local snippets = require("mini.snippets")
-local gen_loader = snippets.gen_loader
 snippets.setup({
-  snippets = { gen_loader.from_lang() },
+  snippets = { snippets.gen_loader.from_lang() },
   mappings = {
     expand = "",
     jump_prev = "<C-h>",
     jump_next = "<C-l>",
     stop = "<C-c>",
   },
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  desc = "stop mini.snippets when leaving insert mode",
+  group = require("augroup"),
+  pattern = "*",
+  callback = function() snippets.session.stop() end,
 })
