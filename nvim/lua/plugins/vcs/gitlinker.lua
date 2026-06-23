c69d93a590b9908c895cda9e33e108d8de1bd9df
Ron Shavit <me@ronshavit.com>
2026-06-23 17:44:45 +0300

    nvim/gitlinker: support tangled.org
    Signed-off-by: Ron Shavit <me@ronshavit.com>

--- nvim/lua/plugins/vcs/gitlinker.lua
+++ nvim/lua/plugins/vcs/gitlinker.lua
@@ -1,21 +1,29 @@
 local gl = require("gitlinker")
+
+local function finish_tangled_url(url, url_data)
+  url = url .. url_data.repo .. "/blob/" .. url_data.rev .. "/" .. url_data.file
+  if url_data.lstart then
+    url = url .. "#L" .. url_data.lstart
+    if url_data.lend then url = url .. "-L" .. url_data.lend end
+  end
+  return url
+end
+
+local function get_tangled_type_url(url_data)
+  if not url_data.repo or not url_data.file then return end
+  return finish_tangled_url("https://tangled.org/", url_data)
+end
+
+local function get_tangled_knot_type_url(url_data)
+  if not url_data.repo or not url_data.file then return end
+  return finish_tangled_url("https://tangled.org/did:plc:", url_data)
+end
+
 gl.setup({
   mappings = nil,
   callbacks = {
-    ["knot..*.com"] = function(url_data)
-      if not url_data.repo or not url_data.file then return end
-      local url = "https://tangled.org/did:plc:"
-        .. url_data.repo
-        .. "/blob/"
-        .. url_data.rev
-        .. "/"
-        .. url_data.file
-      if url_data.lstart then
-        url = url .. "#L" .. url_data.lstart
-        if url_data.lend then url = url .. "-L" .. url_data.lend end
-      end
-      return url
-    end,
+    ["tangled.org"] = get_tangled_type_url,
+    ["knot..*.com"] = get_tangled_knot_type_url,
   },
 })
 
