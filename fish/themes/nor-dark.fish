14f7df6e2d9974aef75e4edc6b06e05a7a9f43c8
Ron Shavit <me@ronshavit.com>
2026-06-29 12:43:47 +0300

    theme changes
    Signed-off-by: Ron Shavit <me@ronshavit.com>

--- fish/themes/nor-dark.fish
+++ fish/themes/nor-dark.fish
@@ -1,26 +1,26 @@
-# nor.nvim ${variant} for fish
+# nor.nvim dark for fish
 # auto generated with https://tangled.org/ronshavit.com/nor.nvim/blob/master/lua/nor/extras/fish.lua
 
 # syntax
-set -g fish_color_normal ffffff
+set -g fish_color_normal c6c6c6
 set -g fish_color_command 5baba9
 set -g fish_color_keyword b84243
 set -g fish_color_quote e26d6d
-set -g fish_color_redirection ffffff
+set -g fish_color_redirection c6c6c6
 set -g fish_color_end 9e9e9e
 set -g fish_color_option de7247
-set -g fish_color_error df3435
+set -g fish_color_error b84243
 set -g fish_color_param ffffff
 set -g fish_color_comment 9e9e9e
 set -g fish_color_selection --background=474747
 set -g fish_color_search_match --background=474747
-set -g fish_color_operator ffffff
+set -g fish_color_operator c6c6c6
 set -g fish_color_escape b84243
 set -g fish_color_autosuggestion 9e9e9e
 
 # pager
 set -g fish_pager_color_progress 9e9e9e
 set -g fish_pager_color_prefix 5baba9
-set -g fish_pager_color_completion ffffff
+set -g fish_pager_color_completion c6c6c6
 set -g fish_pager_color_description 9e9e9e
 set -g fish_pager_color_selected_background --background=474747
--- fish/themes/nor-light.fish
+++ fish/themes/nor-light.fish
@@ -1,26 +1,26 @@
-# nor.nvim ${variant} for fish
+# nor.nvim light for fish
 # auto generated with https://tangled.org/ronshavit.com/nor.nvim/blob/master/lua/nor/extras/fish.lua
 
 # syntax
-set -g fish_color_normal 000000
+set -g fish_color_normal 3b3b3b
 set -g fish_color_command 35656e
 set -g fish_color_keyword db5959
 set -g fish_color_quote c13939
-set -g fish_color_redirection 000000
+set -g fish_color_redirection 3b3b3b
 set -g fish_color_end 6a6a6a
 set -g fish_color_option a8522d
-set -g fish_color_error c92d2f
+set -g fish_color_error db5959
 set -g fish_color_param 000000
 set -g fish_color_comment 6a6a6a
 set -g fish_color_selection --background=d4d4d4
 set -g fish_color_search_match --background=d4d4d4
-set -g fish_color_operator 000000
+set -g fish_color_operator 3b3b3b
 set -g fish_color_escape db5959
 set -g fish_color_autosuggestion 6a6a6a
 
 # pager
 set -g fish_pager_color_progress 6a6a6a
 set -g fish_pager_color_prefix 35656e
-set -g fish_pager_color_completion 000000
+set -g fish_pager_color_completion 3b3b3b
 set -g fish_pager_color_description 6a6a6a
 set -g fish_pager_color_selected_background --background=d4d4d4
