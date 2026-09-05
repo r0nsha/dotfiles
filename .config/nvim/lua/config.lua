680a78d4c96543de53fe53090caf2950ef7a3343
Ron Shavit <me@ronshavit.com>
2026-09-05 13:30:14 +0300

    nvim: use `:bcd` in osc7 autocmd
    Signed-off-by: Ron Shavit <me@ronshavit.com>

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -272,16 +272,11 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 vim.api.nvim_create_autocmd({ "TermRequest" }, {
   desc = "Handles OSC 7 dir change requests",
   callback = function(ev)
-    local val, n = string.gsub(ev.data.sequence, "\027]7;file://[^/]*", "")
+    local dir, n = string.gsub(ev.data.sequence, "\027]7;file://[^/]*", "")
     if n > 0 then
       -- OSC 7: dir-change
-      local dir = val
-      if vim.fn.isdirectory(dir) == 0 then
-        vim.notify("invalid dir: " .. dir)
-        return
-      end
-      vim.b[ev.buf].osc7_dir = dir
-      -- if vim.api.nvim_get_current_buf() == ev.buf then vim.cmd.lcd(dir) end
+      assert(vim.fn.isdirectory(dir) ~= 0, "invalid dir: " .. dir)
+      if vim.api.nvim_get_current_buf() == ev.buf then vim.cmd.bcd(dir) end
     end
   end,
 })
